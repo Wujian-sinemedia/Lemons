774652ee49bf010f065321db6dcfeecab34a322c
Ben Vining <ben.the.vining@gmail.com>
2021-03-06 19:19:54 -0700

    Update bv_SharedCode.h

--- bv_SharedCode/bv_SharedCode.h
+++ bv_SharedCode/bv_SharedCode.h
@@ -38,5 +38,37 @@ namespace gui
     }
     
 }  // namespace gui
+    
+
+namespace dsp
+{
+    
+    inline int periodInSamples (double samplerate, int freqHz)
+    {
+        jassert (freqHz > 0);
+        return juce::roundToInt(samplerate / freqHz);
+    }
+    
+    inline int periodInSamples (double samplerate, float freqHz)
+    {
+        jassert (freqHz > 0.0f);
+        return juce::roundToInt(samplerate / freqHz);
+    }
+    
+    inline int freqFromPeriod (double samplerate, int period)
+    {
+        jassert (period > 0);
+        return juce::roundToInt(samplerate / period);
+    }
+    
+    inline int sampsToMs (double samplerate, int numSamples)
+    {
+        jassert (samplerate > 0.0);
+        return juce::roundToInt((numSamples / samplerate) * 1000.0f);
+    }
+    
+    inline int msToSamps (double samplerate, int ms) { return juce::roundToInt(samplerate / 1000.0f * ms); }
+    
+}  // namespace dsp
 
 }  // namespace bav
