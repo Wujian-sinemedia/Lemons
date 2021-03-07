c82c273d15294ede9265bae6da9cf60ef65e817c
Ben Vining <ben.the.vining@gmail.com>
2021-03-06 23:27:28 -0700

    added fifo wrapped engine w/o midi

--- bv_SharedCode/dsp/FIFOWrappedEngine.cpp
+++ bv_SharedCode/dsp/FIFOWrappedEngine.cpp
@@ -25,13 +25,6 @@ namespace dsp
         jassert (samplerate > 0);
         jassert (blocksize > 0);
         
-        const size_t doubleBlocksize = size_t(internalBlocksize * 2);
-        
-        midiInputCollection .ensureSize (doubleBlocksize);
-        midiOutputCollection.ensureSize (doubleBlocksize);
-        chunkMidiBuffer.ensureSize (doubleBlocksize);
-        midiChoppingBuffer.ensureSize (doubleBlocksize);
-        
         inBuffer.setSize (2, internalBlocksize, true, true, true);
         outBuffer.setSize(2, internalBlocksize, true, true, true);
         inputBuffer.changeSize (2, internalBlocksize * 2);
@@ -48,11 +41,6 @@ namespace dsp
         
         inputBuffer.releaseResources();
         outputBuffer.releaseResources();
-        
-        midiChoppingBuffer.clear();
-        midiInputCollection.clear();
-        midiOutputCollection.clear();
-        chunkMidiBuffer.clear();
         
         release();
     }
@@ -102,6 +90,164 @@ namespace dsp
             AudioBuffer<SampleType> inputProxy  (input.getArrayOfWritePointers(),  input.getNumChannels(), startSample, chunkNumSamples);
             AudioBuffer<SampleType> outputProxy (output.getArrayOfWritePointers(), 2,                      startSample, chunkNumSamples);
             
+            processWrapped (inputProxy, outputProxy, actuallyFadingIn, actuallyFadingOut, isBypassed);
+            
+            startSample += chunkNumSamples;
+            samplesLeft -= chunkNumSamples;
+            
+            actuallyFadingIn  = false;
+            actuallyFadingOut = false;
+        }
+        while (samplesLeft > 0);
+    }
+    
+    template<typename SampleType>
+    void FIFOWrappedEngine<SampleType>::processWrapped (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output,
+                                                        const bool applyFadeIn, const bool applyFadeOut,
+                                                        const bool isBypassed)
+    {
+        const int numNewSamples = input.getNumSamples();
+        
+        jassert (numNewSamples <= internalBlocksize);
+        
+        inputBuffer.pushSamples (input, 0, 0, numNewSamples, 0);
+        
+        if (inputBuffer.numStoredSamples() >= internalBlocksize)  // we have enough samples, render the new chunk
+        {
+            inBuffer.clear();
+            inputBuffer.popSamples (inBuffer, 0, 0, internalBlocksize, 0);
+            
+            if (isBypassed)
+            {
+                for (int chan = 0; chan < 2; ++chan)
+                    outputBuffer.pushSamples (inBuffer, 0, 0, internalBlocksize, chan);
+            }
+            else
+            {
+                AudioBuffer<SampleType> outProxy = AudioBuffer<SampleType> (outBuffer.getArrayOfWritePointers(),
+                                                                            outBuffer.getNumChannels(), 0, internalBlocksize);
+                
+                renderBlock (inBuffer, outProxy);
+                
+                for (int chan = 0; chan < outBuffer.getNumChannels(); ++chan)
+                    outputBuffer.pushSamples (outBuffer, chan, 0, internalBlocksize, chan);
+            }
+        }
+        
+        for (int chan = 0; chan < output.getNumChannels(); ++chan)
+            outputBuffer.popSamples (output, chan, 0, numNewSamples, chan);
+        
+        if (applyFadeIn)
+            output.applyGainRamp (0, numNewSamples, 0.0f, 1.0f);
+        
+        if (applyFadeOut)
+            output.applyGainRamp (0, numNewSamples, 1.0f, 0.0f);
+    }
+    
+    
+    template class FIFOWrappedEngine<float>;
+    template class FIFOWrappedEngine<double>;
+    
+    
+/****************************************************************************************************************************************************
+****************************************************************************************************************************************************/
+    
+    
+    
+    template<typename SampleType>
+    FIFOWrappedEngineWithMidi<SampleType>::FIFOWrappedEngineWithMidi(int initInternalBlocksize): internalBlocksize(initInternalBlocksize)
+    {
+        inputBuffer.initialize(2, internalBlocksize * 2);
+        outputBuffer.initialize(2, internalBlocksize * 2);
+    }
+    
+    template<typename SampleType>
+    FIFOWrappedEngineWithMidi<SampleType>::FIFOWrappedEngineWithMidi() { }
+    
+    
+    template<typename SampleType>
+    void FIFOWrappedEngineWithMidi<SampleType>::prepare (double samplerate, int blocksize)
+    {
+        jassert (samplerate > 0);
+        jassert (blocksize > 0);
+        
+        const size_t doubleBlocksize = size_t(internalBlocksize * 2);
+        
+        midiInputCollection .ensureSize (doubleBlocksize);
+        midiOutputCollection.ensureSize (doubleBlocksize);
+        chunkMidiBuffer.ensureSize (doubleBlocksize);
+        midiChoppingBuffer.ensureSize (doubleBlocksize);
+        
+        inBuffer.setSize (2, internalBlocksize, true, true, true);
+        outBuffer.setSize(2, internalBlocksize, true, true, true);
+        inputBuffer.changeSize (2, internalBlocksize * 2);
+        outputBuffer.changeSize(2, internalBlocksize * 3);
+        
+        prepareToPlay (samplerate, blocksize);
+    }
+    
+    template<typename SampleType>
+    void FIFOWrappedEngineWithMidi<SampleType>::releaseResources()
+    {
+        inBuffer.setSize(0, 0, false, false, false);
+        outBuffer.setSize(0, 0, false, false, false);
+        
+        inputBuffer.releaseResources();
+        outputBuffer.releaseResources();
+        
+        midiChoppingBuffer.clear();
+        midiInputCollection.clear();
+        midiOutputCollection.clear();
+        chunkMidiBuffer.clear();
+        
+        release();
+    }
+    
+    template<typename SampleType>
+    void FIFOWrappedEngineWithMidi<SampleType>::changeLatency (int newInternalBlocksize)
+    {
+        if (internalBlocksize == newInternalBlocksize)
+            return;
+        
+        internalBlocksize = newInternalBlocksize;
+        
+        inBuffer.setSize (2, internalBlocksize, true, true, true);
+        outBuffer.setSize (2, internalBlocksize, true, true, true);
+        
+        inputBuffer.changeSize (2, internalBlocksize * 2);
+        outputBuffer.changeSize(2, internalBlocksize * 3);
+        
+        latencyChanged (newInternalBlocksize);
+    }
+    
+    template<typename SampleType>
+    void FIFOWrappedEngineWithMidi<SampleType>::process (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output, MidiBuffer& midiMessages,
+                                                         const bool applyFadeIn, const bool applyFadeOut,
+                                                         const bool isBypassed)
+    {
+        const int totalNumSamples = input.getNumSamples();
+        
+        if (totalNumSamples == 0 || input.getNumChannels() == 0 || output.getNumSamples() == 0 || output.getNumChannels() == 0)
+            return;
+        
+        if (totalNumSamples <= internalBlocksize)
+        {
+            processWrapped (input, output, midiMessages, applyFadeIn, applyFadeOut, isBypassed);
+            return;
+        }
+        
+        int samplesLeft = totalNumSamples;
+        int startSample = 0;
+        
+        bool actuallyFadingIn  = applyFadeIn;
+        bool actuallyFadingOut = applyFadeOut;
+        
+        do {
+            const int chunkNumSamples = std::min (internalBlocksize, samplesLeft);
+            
+            AudioBuffer<SampleType> inputProxy  (input.getArrayOfWritePointers(),  input.getNumChannels(), startSample, chunkNumSamples);
+            AudioBuffer<SampleType> outputProxy (output.getArrayOfWritePointers(), 2,                      startSample, chunkNumSamples);
+            
             /* put just the midi messages for this time segment into the midiChoppingBuffer
              in the midiChoppingBuffer, events will start at timestamp sample 0
              the harmonizer's midi output will be returned by being copied to this same region of the midiChoppingBuffer */
@@ -123,10 +269,10 @@ namespace dsp
     }
     
     template<typename SampleType>
-    void FIFOWrappedEngine<SampleType>::processWrapped (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output,
-                                                        MidiBuffer& midiMessages,
-                                                        const bool applyFadeIn, const bool applyFadeOut,
-                                                        const bool isBypassed)
+    void FIFOWrappedEngineWithMidi<SampleType>::processWrapped (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output,
+                                                                MidiBuffer& midiMessages,
+                                                                const bool applyFadeIn, const bool applyFadeOut,
+                                                                const bool isBypassed)
     {
         const int numNewSamples = input.getNumSamples();
         
@@ -175,8 +321,8 @@ namespace dsp
     }
     
     
-    template class FIFOWrappedEngine<float>;
-    template class FIFOWrappedEngine<double>;
+    template class FIFOWrappedEngineWithMidi<float>;
+    template class FIFOWrappedEngineWithMidi<double>;
     
     
 }  // namespace dsp
--- bv_SharedCode/dsp/FIFOWrappedEngine.h
+++ bv_SharedCode/dsp/FIFOWrappedEngine.h
@@ -7,6 +7,9 @@ namespace dsp
     
     using namespace juce;
     
+/*
+    Base class for an audio processor wrapped in a FIFO (audio only; for MIDI features, use FIFOWrappedEngineWithMidi).
+*/
 
 template<typename SampleType>
 class FIFOWrappedEngine
@@ -14,6 +17,60 @@ class FIFOWrappedEngine
 public:
     FIFOWrappedEngine(int internalBlocksize);
     virtual ~FIFOWrappedEngine();
+    
+    
+    void process (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output,
+                  const bool applyFadeIn = false, const bool applyFadeOut = false,
+                  const bool isBypassed = false);
+    
+    
+    void prepare (double samplerate, int blocksize);
+    
+    
+    void releaseResources();
+    
+    void changeLatency (int newInternalBlocksize);
+    
+    int getLatency() const noexcept { return internalBlocksize; }
+    
+    
+private:
+    
+    // part of the FIFO process, not for public use
+    void processWrapped (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output,
+                         const bool applyFadeIn, const bool applyFadeOut,
+                         const bool isBypassed = false);
+    
+    virtual void renderBlock (const AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output)=0;
+    
+    virtual void prepareToPlay (double samplerate, int blocksize) { juce::ignoreUnused (samplerate, blocksize); }
+    
+    virtual void release() { }
+    
+    virtual void latencyChanged (int newInternalBlocksize) { juce::ignoreUnused(newInternalBlocksize); }
+    
+    int internalBlocksize; // the size of the processing blocks, in samples, that the algorithm will be processing
+    
+    bav::dsp::AudioFIFO<SampleType> inputBuffer;
+    bav::dsp::AudioFIFO<SampleType> outputBuffer;
+    AudioBuffer<SampleType> inBuffer;
+    AudioBuffer<SampleType> outBuffer;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FIFOWrappedEngine)
+};
+    
+    
+    
+/*
+    Base class for an audio processor wrapped in FIFOs, with audio and midi.
+*/
+
+template<typename SampleType>
+class FIFOWrappedEngineWithMidi
+{
+public:
+    FIFOWrappedEngineWithMidi(int internalBlocksize);
+    virtual FIFOWrappedEngineWithMidi();
     
     
     void process (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output, MidiBuffer& midiMessages,
@@ -33,6 +90,7 @@ public:
     
 private:
     
+    // part of the FIFO process, not for public use
     void processWrapped (AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output,
                          MidiBuffer& midiMessages,
                          const bool applyFadeIn, const bool applyFadeOut,
@@ -58,7 +116,7 @@ private:
     AudioBuffer<SampleType> inBuffer;
     AudioBuffer<SampleType> outBuffer;
     
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FIFOWrappedEngine)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FIFOWrappedEngineWithMidi)
 };
     
 }  // namespace dsp
