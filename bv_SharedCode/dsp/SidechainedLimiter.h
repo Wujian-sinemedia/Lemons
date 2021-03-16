791d5e965418214b0be1399869cd8efa50c908ba
Ben Vining <ben.the.vining@gmail.com>
2021-03-16 00:38:46 -0700

    Update SidechainedLimiter.h

--- bv_SharedCode/dsp/SidechainedLimiter.h
+++ bv_SharedCode/dsp/SidechainedLimiter.h
@@ -15,22 +15,36 @@ namespace dsp
         
         void setThreshold (float thresh_dB)
         {
-            
+            thresholddB = SampleType(thresh_dB);
+            update();
         }
         
         void setRelease (float release_ms)
         {
-            
+            releaseTime = SampleType(release_ms);
+            update();
         }
         
         void prepare (int blocksize, double samplerate, int numChannels)
         {
+            jassert (samplerate > 0);
+            jassert (numChannels > 0);
             
+            sampleRate = samplerate;
+            
+            firstStageCompressor.prepare (blocksize, samplerate, numChannels);
+            secondStageCompressor.prepare (blocksize, samplerate, numChannels);
+            
+            update();
+            reset();
         }
         
         void reset()
         {
+            firstStageCompressor.reset();
+            secondStageCompressor.reset();
             
+            outputVolume.reset (sampleRate, 0.001);
         }
         
         
@@ -53,15 +67,51 @@ namespace dsp
         }
         
         
-        void process (const SampleType* sidechain,
+        void process (const int channel,
+                      const SampleType* sidechain,
                       SampleType* signalToLimit,
-                      int numSamples)
+                      const int numSamples)
         {
             
+            firstStageCompressor.process (channel, sidechain, signalToLimit, numSamples);
+            secondStageCompressor.process (channel, sidechain, signalToLimit, numSamples);
+            
+            juce::FloatVectorOperations::multiply (signalToLimit, outputVolume);
+        
+            juce::FloatVectorOperations::clip (signalToLimit, signalToLimit, SampleType(-1.0), SampleType(1.0), numSamples);
         }
         
         
     private:
+        
+        void update()
+        {
+            firstStageCompressor.setThreshold (-10.0f);
+            firstStageCompressor.setRatio     (4.0f);
+            firstStageCompressor.setAttack    (2.0f);
+            firstStageCompressor.setRelease   (200.0f);
+            
+            secondStageCompressor.setThreshold (thresholddB);
+            secondStageCompressor.setRatio     (1000.0f);
+            secondStageCompressor.setAttack    (0.001f);
+            secondStageCompressor.setRelease   (releaseTime);
+            
+            auto ratioInverse = (SampleType) (1.0 / 4.0);
+            
+            auto gain = (SampleType) std::pow (10.0, 10.0 * (1.0 - ratioInverse) / 40.0);
+            gain *= Decibels::decibelsToGain (-thresholddB, (SampleType) -100.0);
+            
+            outputVolume.setTargetValue (gain);
+        }
+        
+        
+        SidechainedCompressor<SampleType> firstStageCompressor, secondStageCompressor;
+        
+        SmoothedValue<SampleType, ValueSmoothingTypes::Linear> outputVolume;
+        
+        double sampleRate = 44100.0;
+        float thresholddB = -10.0, releaseTime = 100.0;
+        
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SidechainedLimiter)
     };
     
