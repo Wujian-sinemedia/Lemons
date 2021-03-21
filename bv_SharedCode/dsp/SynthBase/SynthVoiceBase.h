19b9d29924ae7740f6ed71f66aae7cf45832cbbd
Ben Vining <ben.the.vining@gmail.com>
2021-03-21 16:39:23 -0700

    Update SynthVoiceBase.h

--- bv_SharedCode/dsp/SynthBase/SynthVoiceBase.h
+++ bv_SharedCode/dsp/SynthBase/SynthVoiceBase.h
@@ -1,21 +1,17 @@
 
-/*
-    Base class for one voice that can be used by the SynthBase to play polyphonically
-*/
-
 
 namespace bav::dsp
 {
 
 
-// multiplicative smoothing cannot ever actually reach 0
-#define bv_MIN_SMOOTHED_GAIN 0.0000001
-#define _SMOOTHING_ZERO_CHECK(inputGain) std::max(SampleType(bv_MIN_SMOOTHED_GAIN), SampleType (inputGain))
-
-
 /// forward declaration...
 template<typename SampleType>
 class SynthBase;
+    
+    
+/*
+    Base class for one voice that can be used by the SynthBase to play polyphonically
+*/
 
 
 template<typename SampleType>
@@ -26,13 +22,162 @@ class SynthVoiceBase
     using ADSRParams = juce::ADSR::Parameters;
     using AudioBuffer = juce::AudioBuffer<SampleType>;
     
+    /*=================================================================================
+     =================================================================================*/
+    
 public:
-    SynthVoiceBase(SynthBase<SampleType>* base, double initSamplerate = 44100.0):
-    parent(base),
-    keyIsDown(false), playingButReleased(false), sustainingFromSostenutoPedal(false), isQuickFading(false), noteTurnedOff(false),
-    currentlyPlayingNote(-1), currentAftertouch(0), currentOutputFreq(-1.0f),  lastRecievedVelocity(0.0f), noteOnTime(0),
-    isPedalPitchVoice(false), isDescantVoice(false),
-    renderingBuffer(0, 0), stereoBuffer(0, 0)
+    SynthVoiceBase(SynthBase<SampleType>* base, double initSamplerate = 44100.0);
+    
+    virtual ~SynthVoiceBase() = default;
+    
+    /*
+     */
+    
+    // prepare is virtual, because if you don't use the default renderBlock, you may not need to initialize the renderingBuffer.
+    virtual void prepare (const int blocksize);
+    
+    // this function only redirects to the subclass's released(), if it's been overridden.
+    void release() { released(); }
+    
+    virtual void renderBlock (AudioBuffer& output);
+    
+    void bypassedBlock (const int numSamples);
+    
+    void resetRampedValues (int blocksize);
+    
+    void setCurrentOutputFreq (const float newFreq) { currentOutputFreq = newFreq; }
+    
+    bool isCurrentPedalVoice()   const noexcept { return isPedalPitchVoice; }
+    bool isCurrentDescantVoice() const noexcept { return isDescantVoice; }
+    
+    int getCurrentMidiPan() const noexcept { return panner.getLastMidiPan(); }
+    
+    float getLastRecievedVelocity() const noexcept { return lastRecievedVelocity; }
+    
+    bool isKeyDown() const noexcept { return keyIsDown; }
+    
+    bool wasStartedBefore (const SynthVoiceBase& other) const noexcept { return noteOnTime < other.noteOnTime; }
+    
+    bool isPlayingButReleased()   const noexcept { return playingButReleased; }
+    
+    bool isVoiceActive() const noexcept { return (currentlyPlayingNote >= 0); }
+    
+    int getCurrentlyPlayingNote() const noexcept { return currentlyPlayingNote; }
+    
+    
+    /*=================================================================================
+     =================================================================================*/
+    
+protected:
+    friend class SynthBase<SampleType>;
+    
+    // this function resets the voice's internal state & marks it as avaiable to accept a new note
+    void clearCurrentNote();
+    
+    
+    SynthBase<SampleType>* parent;  // pointer to the SynthBase object that manages this voice. Make sure you do not delete the parent object before the child!
+    
+    ADSR adsr;         // the main/primary ADSR driven by MIDI input to shape the voice's amplitude envelope. May be turned off by the user.
+    ADSR quickRelease; // used to quickly fade out signal when stopNote() is called with the allowTailOff argument set to false, instead of jumping signal to 0
+    ADSR quickAttack;  // used for if normal ADSR user toggle is OFF, to prevent jumps/pops at starts of notes.
+    
+    bool keyIsDown, playingButReleased, sustainingFromSostenutoPedal, isQuickFading, noteTurnedOff;
+    
+    int currentlyPlayingNote, currentAftertouch;
+    float currentOutputFreq, lastRecievedVelocity;
+    
+    uint32 noteOnTime;
+    
+    bool isPedalPitchVoice, isDescantVoice;
+    
+    
+    /*=================================================================================
+     =================================================================================*/
+    
+private:
+    virtual void renderPlease (AudioBuffer& output, float desiredFrequency, double currentSamplerate)
+    {
+        juce::ignoreUnused (output, desiredFrequency, currentSamplerate);
+    }
+    
+    // if overridden, called in the subclass when the top-level call to release() is made
+    virtual void released() { }
+    
+    // if overridden, called in the subclass any time clearCurrentNote() is called
+    virtual void noteCleared() { }
+    
+    // if overridden, called in the subclass when the top-level call to bypassedBlock() is made.
+    virtual void bypassedBlockRecieved (int numSamples) { juce::ignoreUnused (numSamples); }
+    
+    void startNote (const int midiPitch,  const float velocity,
+                    const uint32 noteOnTimestamp,
+                    const bool keyboardKeyIsDown = true,
+                    const bool isPedal = false, const bool isDescant = false);
+    
+    void stopNote (const float velocity, const bool allowTailOff);
+    
+    void updateSampleRate (const double newSamplerate);
+    
+    void setKeyDown (bool isNowDown);
+    
+    void setPan (int newPan);
+    
+    void setVelocityMultiplier (const float newMultiplier);
+    
+    void softPedalChanged (bool isDown);
+    
+    void aftertouchChanged (const int newAftertouchValue);
+    
+    void setAdsrParameters (const ADSRParams newParams) { adsr.setParameters(newParams); }
+    void setQuickReleaseParameters (const ADSRParams newParams) { quickRelease.setParameters(newParams); }
+    void setQuickAttackParameters  (const ADSRParams newParams) { quickAttack.setParameters(newParams); }
+    
+    /*=================================================================================
+     =================================================================================*/
+    
+    bav::dsp::Panner panner;
+    
+    // gain smoothers
+    juce::SmoothedValue<SampleType, juce::ValueSmoothingTypes::Multiplicative> midiVelocityGain, softPedalGain, playingButReleasedGain, outputLeftGain, outputRightGain, aftertouchGain;
+    
+    AudioBuffer renderingBuffer;  // mono audio will be placed in here
+    AudioBuffer stereoBuffer;     // stereo audio will be placed in here
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SynthVoiceBase)
+};
+
+    
+    /*=================================================================================
+     =================================================================================*/
+
+
+//==============================================================================
+//        _        _           _  _
+//     __| |  ___ | |_   __ _ (_)| | ___
+//    / _` | / _ \| __| / _` || || |/ __|
+//   | (_| ||  __/| |_ | (_| || || |\__ \ _  _  _
+//    \__,_| \___| \__| \__,_||_||_||___/(_)(_)(_)
+//
+//   Beyond here be implentation details.....
+//
+//==============================================================================
+
+
+// multiplicative smoothing cannot ever actually reach 0
+#define bv_MIN_SMOOTHED_GAIN 0.0000001
+#define _SMOOTHING_ZERO_CHECK(inputGain) std::max(SampleType(bv_MIN_SMOOTHED_GAIN), SampleType (inputGain))
+ 
+
+    /*
+        Constructor requires a pointer to the parent SynthBase object that manages this voice.
+    */
+    template<typename SampleType>
+    SynthVoiceBase<SampleType>::SynthVoiceBase<SampleType>(SynthBase<SampleType>* base, double initSamplerate = 44100.0):
+            parent(base),
+            keyIsDown(false), playingButReleased(false), sustainingFromSostenutoPedal(false), isQuickFading(false), noteTurnedOff(false),
+            currentlyPlayingNote(-1), currentAftertouch(0), currentOutputFreq(-1.0f),  lastRecievedVelocity(0.0f), noteOnTime(0),
+            isPedalPitchVoice(false), isDescantVoice(false),
+            renderingBuffer(0, 0), stereoBuffer(0, 0)
     {
         adsr        .setSampleRate (initSamplerate);
         quickRelease.setSampleRate (initSamplerate);
@@ -42,23 +187,17 @@ public:
         quickAttack .setParameters (parent->getCurrentQuickAttackParams());
     }
     
-    virtual ~SynthVoiceBase()
-    { }
     
-    /*=================================================================================
-     =================================================================================*/
-    
-    // prepare is virtual, because if you don't use the default renderBlock, you may not need to initialize the renderingBuffer.
-    virtual void prepare (const int blocksize)
+    template<typename SampleType>
+    virtual void SynthVoiceBase<SampleType>::prepare (const int blocksize)
     {
         renderingBuffer.setSize (1, blocksize, true, true, true);
         stereoBuffer.setSize (2, blocksize, true, true, true);
     }
     
-    void release() { released(); }
     
-    
-    virtual void renderBlock (AudioBuffer& output)
+    template<typename SampleType>
+    virtual void SynthVoiceBase<SampleType>::renderBlock (AudioBuffer& output)
     {
         const bool voiceIsOnRightNow = isQuickFading ? quickRelease.isActive()
                                                      : ( parent->isADSRon() ? adsr.isActive() : ! noteTurnedOff );
@@ -68,8 +207,7 @@ public:
             return;
         }
         
-        jassert (output.getNumSamples() > 0);
-        jassert (output.getNumChannels() > 0);
+        jassert (output.getNumSamples() > 0 && output.getNumChannels() > 0);
         
         jassert (currentOutputFreq > 0);
         jassert (parent->getSamplerate() > 0);
@@ -115,7 +253,8 @@ public:
     }
     
     
-    void bypassedBlock (const int numSamples)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::bypassedBlock (const int numSamples)
     {
         midiVelocityGain.skip (numSamples);
         softPedalGain.skip (numSamples);
@@ -126,7 +265,9 @@ public:
         bypassedBlockRecieved (numSamples);
     }
     
-    void resetRampedValues (int blocksize)
+    
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::resetRampedValues (int blocksize)
     {
         midiVelocityGain.reset (blocksize);
         softPedalGain.reset (blocksize);
@@ -136,31 +277,9 @@ public:
         outputRightGain.reset (blocksize);
     }
     
-    bool isCurrentPedalVoice()   const noexcept { return isPedalPitchVoice; }
-    bool isCurrentDescantVoice() const noexcept { return isDescantVoice; }
     
-    int getCurrentMidiPan() const noexcept { return panner.getLastMidiPan(); }
-    
-    float getLastRecievedVelocity() const noexcept { return lastRecievedVelocity; }
-    
-    bool isKeyDown() const noexcept { return keyIsDown; }
-    
-    bool wasStartedBefore (const SynthVoiceBase& other) const noexcept { return noteOnTime < other.noteOnTime; }
-    
-    bool isPlayingButReleased()   const noexcept { return playingButReleased; }
-    
-    bool isVoiceActive() const noexcept { return (currentlyPlayingNote >= 0); }
-    
-    int getCurrentlyPlayingNote() const noexcept { return currentlyPlayingNote; }
-    
-    void setCurrentOutputFreq (const float newFreq) { currentOutputFreq = newFreq; }
-    
-    
-protected:
-    friend class SynthBase<SampleType>;
-    
-    // this function resets the voice's internal state & marks it as avaiable to accept a new note
-    void clearCurrentNote()
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::clearCurrentNote()
     {
         lastRecievedVelocity = 0.0f;
         currentAftertouch = 0;
@@ -193,44 +312,11 @@ protected:
     }
     
     
-    SynthBase<SampleType>* parent;  // pointer to the SynthBase object that manages this voice. Make sure you do not delete the parent object before the child!
-    
-    
-    ADSR adsr;         // the main/primary ADSR driven by MIDI input to shape the voice's amplitude envelope. May be turned off by the user.
-    ADSR quickRelease; // used to quickly fade out signal when stopNote() is called with the allowTailOff argument set to false, instead of jumping signal to 0
-    ADSR quickAttack;  // used for if normal ADSR user toggle is OFF, to prevent jumps/pops at starts of notes.
-    
-    bool keyIsDown, playingButReleased, sustainingFromSostenutoPedal, isQuickFading, noteTurnedOff;
-    
-    int currentlyPlayingNote, currentAftertouch;
-    float currentOutputFreq, lastRecievedVelocity;
-    
-    uint32 noteOnTime;
-    
-    bool isPedalPitchVoice, isDescantVoice;
-    
-    
-private:
-    
-    virtual void renderPlease (AudioBuffer& output, float desiredFrequency, double currentSamplerate)
-    {
-        juce::ignoreUnused (output, desiredFrequency, currentSamplerate);
-    }
-    
-    // if overridden, called in the subclass when the top-level call to release() is made
-    virtual void released() { }
-    
-    // if overridden, called in the subclass any time clearCurrentNote() is called
-    virtual void noteCleared() { }
-    
-    // if overridden, called in the subclass when the top-level call to bypassedBlock() is made.
-    virtual void bypassedBlockRecieved (int numSamples) { juce::ignoreUnused (numSamples); }
-    
-    
-    void startNote (const int midiPitch,  const float velocity,
-                    const uint32 noteOnTimestamp,
-                    const bool keyboardKeyIsDown = true,
-                    const bool isPedal = false, const bool isDescant = false)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::startNote (const int midiPitch,  const float velocity,
+                                                const uint32 noteOnTimestamp,
+                                                const bool keyboardKeyIsDown = true,
+                                                const bool isPedal = false, const bool isDescant = false)
     {
         noteOnTime = noteOnTimestamp;
         currentlyPlayingNote = midiPitch;
@@ -253,7 +339,8 @@ private:
     }
     
     
-    void stopNote (const float velocity, const bool allowTailOff)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::stopNote (const float velocity, const bool allowTailOff)
     {
         midiVelocityGain.setTargetValue (_SMOOTHING_ZERO_CHECK (parent->getWeightedVelocity (lastRecievedVelocity - velocity)));
         
@@ -278,7 +365,8 @@ private:
     }
     
     
-    void updateSampleRate (const double newSamplerate)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::updateSampleRate (const double newSamplerate)
     {
         adsr        .setSampleRate (newSamplerate);
         quickRelease.setSampleRate (newSamplerate);
@@ -290,7 +378,8 @@ private:
     }
     
     
-    void setKeyDown (bool isNowDown)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::setKeyDown (bool isNowDown)
     {
         keyIsDown = isNowDown;
         
@@ -313,7 +402,8 @@ private:
     }
     
     
-    void setPan (int newPan)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::setPan (int newPan)
     {
         newPan = juce::jlimit(0, 127, newPan);
         
@@ -327,10 +417,8 @@ private:
     }
     
     
-    void setVelocityMultiplier (const float newMultiplier) { midiVelocityGain.setTargetValue (_SMOOTHING_ZERO_CHECK(newMultiplier)); }
-    
-    
-    void softPedalChanged (bool isDown)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::softPedalChanged (bool isDown)
     {
         if (isDown)
             softPedalGain.setTargetValue (_SMOOTHING_ZERO_CHECK (parent->getSoftPedalMultiplier()));
@@ -339,7 +427,8 @@ private:
     }
     
     
-    void aftertouchChanged (const int newAftertouchValue)
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::aftertouchChanged (const int newAftertouchValue)
     {
         currentAftertouch = newAftertouchValue;
         
@@ -351,26 +440,21 @@ private:
             aftertouchGain.setTargetValue (SampleType(1.0));
     }
     
-    void setAdsrParameters (const ADSRParams newParams) { adsr.setParameters(newParams); }
-    void setQuickReleaseParameters (const ADSRParams newParams) { quickRelease.setParameters(newParams); }
-    void setQuickAttackParameters  (const ADSRParams newParams) { quickAttack.setParameters(newParams); }
     
-    bav::dsp::Panner panner;
-    
-    // gain smoothers
-    juce::SmoothedValue<SampleType, juce::ValueSmoothingTypes::Multiplicative> midiVelocityGain, softPedalGain, playingButReleasedGain, outputLeftGain, outputRightGain, aftertouchGain;
-    
-    AudioBuffer renderingBuffer;  // mono audio will be placed in here
-    AudioBuffer stereoBuffer;     // stereo audio will be placed in here
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SynthVoiceBase)
-};
-
-}  // namespace
+    template<typename SampleType>
+    void SynthVoiceBase<SampleType>::setVelocityMultiplier (const float newMultiplier)
+    {
+        midiVelocityGain.setTargetValue (_SMOOTHING_ZERO_CHECK(newMultiplier));
+    }
 
 
 #undef bv_MIN_SMOOTHED_GAIN
 #undef _SMOOTHING_ZERO_CHECK
 
+    
+    ///explicit class instantiations
+    template class SynthVoiceBase<float>;
+    template class SynthVoiceBase<double>;
+    
 
-
+}  // namespace
