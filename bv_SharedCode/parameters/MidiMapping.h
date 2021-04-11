7bdcfa2004c68be35b03ecf0f3216d674e28c157
Ben Vining <ben.the.vining@gmail.com>
2021-04-10 21:49:43 -0700

    Update MidiMapping.h

--- bv_SharedCode/parameters/MidiMapping.h
+++ bv_SharedCode/parameters/MidiMapping.h
@@ -9,7 +9,10 @@ namespace bav
     class MidiCC_Listener
     {
     public:
-        MidiCC_Listener(Parameter* param, int controller): parameter(param), controllerNum(controller)
+        MidiCC_Listener(Parameter* param, int controller)
+            : parameter(param),
+              controllerNum(controller),
+              lastControllerValue(defaultLastControllerVal)
         {
             jassert (parameter != nullptr);
         }
@@ -27,19 +30,31 @@ namespace bav
             constexpr auto inv127 = 1.0f / 127.0f;
             
             if (controllerNumber == controllerNum)
-                parameter->changeValueNotifyHost (parameter->normalize (controllerValue * inv127));
+            {
+                parameter->changeValueNotifyHost (controllerValue * inv127);
+                lastControllerValue = controllerValue;
+            }
         }
         
-        void changeControllerNumber (int newControllerNumber) noexcept { controllerNum = newControllerNumber; }
+        void changeControllerNumber (int newControllerNumber) noexcept
+        {
+            controllerNum = newControllerNumber;
+            lastControllerValue = defaultLastControllerVal;
+        }
         
         Parameter* getParameter() const noexcept { return parameter; }
         
         int getControllerNumber() const noexcept { return controllerNum; }
         
+        int getLastControllerValue() const noexcept { return lastControllerValue; }
+        
         
     private:
         Parameter* const parameter;
         int controllerNum;
+        int lastControllerValue;
+        
+        static constexpr int defaultLastControllerVal = 64;
     };
     
     
@@ -62,8 +77,18 @@ namespace bav
             mappings.add (new MidiCC_Listener (parameter, controllerNumber));
         }
         
+        /* clears any previous mappings this object held and loads the new specified set of mappings */
+        void loadMappingSet (MidiCC_Listener* parameterMappings, int numMappings)
+        {
+            mappings.clearQuick();
+            mappings.ensureStorageAllocated (numMappings);
+            
+            for (int i = 0; i < numMappings; ++i)
+                mappings.add (std::move (parameterMappings[i]));
+        }
+        
         /* if a mapping exists for the current parameter, it will be changed to now listen for the new CC number. If no mapping exists for the parameter, returns false. */
-        bool changeParameterMapping (Parameter* parameter, int newControllerNumber)
+        bool changeParameterMapping (const Parameter* parameter, int newControllerNumber)
         {
             for (auto* mapping : mappings)
             {
@@ -77,7 +102,7 @@ namespace bav
         }
         
         /* removes all mappings to the passed parameter */
-        void removeAllParameterMappingsFor (Parameter* parameter)
+        void removeAllParameterMappingsFor (const Parameter* parameter)
         {
             for (auto* mapping : mappings)
                 if (mapping->getParameter() == parameter)
@@ -93,16 +118,22 @@ namespace bav
         }
         
         /* removes only mappings that map the passed parameter to the passed CC nnumber */
-        void removeParameterMapping (Parameter* parameter, int controllerNumber)
+        void removeParameterMapping (const Parameter* parameter, int controllerNumber)
         {
             for (auto* mapping : mappings)
                 if (mapping->getParameter() == parameter && mapping->getControllerNumber() == controllerNumber)
                     mappings.removeObject (mapping);
         }
         
+        /* clears all active parameter mappings */
+        void clearAllMappings()
+        {
+            mappings.clearQuick();
+        }
+        
         
         /* processes all the MIDI CC events in the passed midi buffer */
-        void processMidiBuffer (juce::MidiBuffer& midiMessages)
+        void processMidiBuffer (const juce::MidiBuffer& midiMessages)
         {
             std::for_each (midiMessages.cbegin(),
                            midiMessages.cend(),
@@ -121,6 +152,26 @@ namespace bav
         {
             for (auto* mapping : mappings)
                 mapping->processNewControllerMessage (controllerNumber, controllerValue);
+        }
+        
+        /* if the parameter is currently mapped, returns a pointer to its MidiCC_Listener; else returns a nullptr */
+        MidiCC_Listener* getMappingForParameter (const Parameter* parameter)
+        {
+            for (auto* mapping : mappings)
+                if (mapping->getParameter() == parameter)
+                    return mapping;
+            
+            return nullptr;
+        }
+        
+        /* if the passed MIDI controller number is mapped, returns a pointer to its MidiCC_Listener; else returns a nullptr */
+        MidiCC_Listener* getMappingForController (int midiControllerNumber)
+        {
+            for (auto* mapping : mappings)
+                if (mapping->getControllerNumber() == midiControllerNumber)
+                    return mapping;
+            
+            return nullptr;
         }
         
         
