44bf48fbc6319a0ba473d65599b670db5c536089
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-20 21:39:11 -0700

    Update Parameters.h

--- bv_SharedCode/parameters/Parameters.h
+++ bv_SharedCode/parameters/Parameters.h
@@ -13,6 +13,36 @@ namespace bav
     }
     
     
+    static inline int midiPanStringToInt (const juce::String& string)
+    {
+        if (string.endsWithIgnoreCase ("C"))
+            return 64;
+        
+        if (string.endsWithIgnoreCase ("R"))
+            return juce::jmap (string.dropLastCharacters(1).getIntValue(), 1, 50, 65, 127);
+        
+        if (string.endsWithIgnoreCase ("L"))
+            return juce::jmap (string.dropLastCharacters(1).getIntValue(), 1, 50, 63, 0);
+        
+        return string.getIntValue();
+    }   
+    
+    static inline juce::String midiPanIntToString (const int midiPan)
+    {
+        if (midiPan == 64)
+            return juce::String ("C");
+        
+        if (midiPan > 64)
+        { 
+            const auto amtRight = juce::jmap (midiPan, 65, 127, 1, 50);
+            return juce::String (amtRight) + "R";
+        }
+        
+        const auto amtLeft = juce::jmap (midiPan, 63, 0, 1, 50);
+        return juce::String (amtLeft) + "L";
+    }    
+    
+    
     
     /*
         My wrapper classes for each individual parameter type all inherit from this base class.
