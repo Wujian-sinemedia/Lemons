a620ea751697555a2b2bc5e28306043720e0f2a0
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-21 01:51:16 -0700

    Update MidiUtilities.h

--- bv_SharedCode/midi/MidiUtilities.h
+++ bv_SharedCode/midi/MidiUtilities.h
@@ -7,6 +7,86 @@
 
 namespace bav::midi
 {
+    // converts a midi pitch to a string representation, eg note 69 = A4
+    inline juce::String pitchToString (const int midiNoteNumber, const bool asSharps = true)
+    {
+        jassert (midiNoteNumber >= 0 && midiNoteNumber <= 127);
+     
+        const auto pitchClass = midiNoteNumber % 12;
+     
+        constexpr auto inv12 = 1.0f / 12.0f;
+        const auto octave = juce::roundToInt (float(midiNoteNumber) * inv12) - 1;
+     
+        static const char* const sharpNoteNames[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        static const char* const flatNoteNames[]  = { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
+        auto noteName = asSharps ? juce::String (sharpNoteNames[pitchClass]) : juce::String (flatNoteNames[pitchClass]);
+     
+        return noteName + juce::String (octave);
+    }
+ 
+    // converts a pitch class string to an integer representation, eg A4 = note 69
+    inline int stringToPitch (const juce::String& string, const bool asSharps = true)
+    {
+        const auto octaveName = string.retainCharacters ("0123456789");
+     
+        const auto pitchClassTokens = juce::String("abcdefg#") + gui::getSharpSymbol() + gui::getFlatSymbol() + gui::getNaturalSymbol();
+        const auto pitchClassName = string.toLowerCase().retainCharacters (pitchClassTokens);
+     
+        const auto octave = octaveName.getIntValue();
+        const auto pitchClass = getPitchClass (pitchClassName);
+        jassert (pitchClass > -1);
+     
+        return (octave * 12) + pitchClass;
+    }
+ 
+ 
+    /*  Returns the pitch class number for a given string.
+        If the string is not in the required format i.e. A#4 etc. this
+        will return -1.
+     */
+    inline int getPitchClass (const juce::String& pitchClassName) noexcept
+    {
+        int pitchClass = -1;
+        const auto numChars = pitchClassName.length();
+        
+        if (numChars > 0)
+        {
+            const auto base = pitchClassName.toLowerCase()[0];
+
+            switch (base)
+            {
+                case 'c':   pitchClass = 0;     break;
+                case 'd':   pitchClass = 2;     break;
+                case 'e':   pitchClass = 4;     break;
+                case 'f':   pitchClass = 5;     break;
+                case 'g':   pitchClass = 7;     break;
+                case 'a':   pitchClass = 9;     break;
+                case 'b':   pitchClass = 11;    break;
+                default:    pitchClass = -1;    break;
+            }
+        }
+        
+        if (numChars > 1)
+        {
+            const auto sharpOrFlat = pitchClassName[1];
+            
+            switch (sharpOrFlat)
+            {
+                case '#':   ++pitchClass;       break;
+                case 'b':   --pitchClass;       break;
+                default:                        break;
+            }
+
+            if (sharpOrFlat == gui::getSharpSymbol())
+                ++pitchClass;
+            else if (sharpOrFlat == gui::getFlatSymbol())
+                --pitchClass;
+            
+            pitchClass %= 12;
+        }
+        
+        return pitchClass;
+    }
     
     
     // returns true if the given midinote is a black key
