39937d750bda19acf9958a8e6d4c0b217e074d36
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-21 02:19:47 -0700

    Update MidiUtilities.h

--- bv_SharedCode/midi/MidiUtilities.h
+++ bv_SharedCode/midi/MidiUtilities.h
@@ -7,44 +7,11 @@
 
 namespace bav::midi
 {
-    // converts a midi pitch to a string representation, eg note 69 = A4
-    inline juce::String pitchToString (const int midiNoteNumber, const bool asSharps = true)
-    {
-        jassert (midiNoteNumber >= 0 && midiNoteNumber <= 127);
-     
-        const auto pitchClass = midiNoteNumber % 12;
-     
-        constexpr auto inv12 = 1.0f / 12.0f;
-        const auto octave = juce::roundToInt (float(midiNoteNumber) * inv12) - 1;
-     
-        static const char* const sharpNoteNames[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-        static const char* const flatNoteNames[]  = { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
-        auto noteName = asSharps ? juce::String (sharpNoteNames[pitchClass]) : juce::String (flatNoteNames[pitchClass]);
-     
-        return noteName + juce::String (octave);
-    }
- 
-    // converts a pitch class string to an integer representation, eg A4 = note 69
-    inline int stringToPitch (const juce::String& string, const bool asSharps = true)
-    {
-        const auto octaveName = string.retainCharacters ("0123456789");
-     
-        const auto pitchClassTokens = juce::String("abcdefg#") + gui::getSharpSymbol() + gui::getFlatSymbol() + gui::getNaturalSymbol();
-        const auto pitchClassName = string.toLowerCase().retainCharacters (pitchClassTokens);
-     
-        const auto octave = octaveName.getIntValue();
-        const auto pitchClass = getPitchClass (pitchClassName);
-        jassert (pitchClass > -1);
-     
-        return (octave * 12) + pitchClass;
-    }
- 
- 
     /*  Returns the pitch class number for a given string.
         If the string is not in the required format i.e. A#4 etc. this
         will return -1.
      */
-    inline int getPitchClass (const juce::String& pitchClassName) noexcept
+    inline int pitchClassFromString (const juce::String& pitchClassName) noexcept
     {
         int pitchClass = -1;
         const auto numChars = pitchClassName.length();
@@ -87,8 +54,40 @@ namespace bav::midi
         
         return pitchClass;
     }
-    
-    
+ 
+    // converts a midi pitch to a string representation, eg note 69 = A4
+    inline juce::String pitchToString (const int midiNoteNumber, const bool asSharps = true)
+    {
+        jassert (midiNoteNumber >= 0 && midiNoteNumber <= 127);
+     
+        const auto pitchClass = midiNoteNumber % 12;
+     
+        constexpr auto inv12 = 1.0f / 12.0f;
+        const auto octave = juce::roundToInt (float(midiNoteNumber) * inv12) - 1;
+     
+        static const char* const sharpNoteNames[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        static const char* const flatNoteNames[]  = { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
+        auto noteName = asSharps ? juce::String (sharpNoteNames[pitchClass]) : juce::String (flatNoteNames[pitchClass]);
+     
+        return noteName + juce::String (octave);
+    }
+ 
+    // converts a pitch class string to an integer representation, eg A4 = note 69
+    inline int stringToPitch (const juce::String& string, const bool asSharps = true)
+    {
+        const auto octaveName = string.retainCharacters ("0123456789");
+     
+        const auto pitchClassTokens = juce::String("abcdefg#") + gui::getSharpSymbol() + gui::getFlatSymbol() + gui::getNaturalSymbol();
+        const auto pitchClassName = string.toLowerCase().retainCharacters (pitchClassTokens);
+     
+        const auto octave = octaveName.getIntValue();
+        const auto pitchClass = pitchClassFromString (pitchClassName);
+        jassert (pitchClass > -1);
+     
+        return (octave * 12) + pitchClass;
+    }
+ 
+ 
     // returns true if the given midinote is a black key
     template<typename noteType>
     inline bool isMidiNoteBlackKey (noteType midipitch)
