ddde2ba62243b3508521e11d5438685389fda45c
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-09 22:49:40 -0700

    Update DummyAudioProcessor.h

--- bv_SharedCode/dsp/DummyAudioProcessor.h
+++ bv_SharedCode/dsp/DummyAudioProcessor.h
@@ -6,36 +6,36 @@ namespace bav::dsp
 class DummyAudioProcessor    : public  juce::AudioProcessor
 {
 public:
-    DummyAudioProcessor();
-    ~DummyAudioProcessor() override;
+    DummyAudioProcessor() { }
+    ~DummyAudioProcessor() override { }
     
     /*=========================================================================================*/
     
-    void prepareToPlay (double, int) override final { }
-    void releaseResources() override final { }
+    void prepareToPlay (double, int) override { }
+    void releaseResources() override { }
     
-    void processBlock (juce::AudioBuffer<float>&,  juce::MidiBuffer&) override final { }
+    void processBlock (juce::AudioBuffer<float>&,  juce::MidiBuffer&) override { }
     
-    double getTailLengthSeconds() const override final;
+    double getTailLengthSeconds() const override { }
     
-    void getStateInformation (juce::MemoryBlock&) override final { }
-    void setStateInformation (const void*, int) override final { }
+    void getStateInformation (juce::MemoryBlock&) override { }
+    void setStateInformation (const void*, int) override { }
     
-    int getNumPrograms() override final { return 1; }
-    int getCurrentProgram() override final { return 0; }
-    void setCurrentProgram (int) override final { }
-    const juce::String getProgramName (int) override final { return {}; }
-    void changeProgramName (int, const juce::String&) override final { }
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int) override { }
+    const juce::String getProgramName (int) override { return {}; }
+    void changeProgramName (int, const juce::String&) override { }
     
-    bool acceptsMidi()  const override final { return true;  }
-    bool producesMidi() const override final { return true;  }
-    bool supportsMPE()  const override final { return false; }
-    bool isMidiEffect() const override final { return false; }
+    bool acceptsMidi()  const override { return true;  }
+    bool producesMidi() const override { return true;  }
+    bool supportsMPE()  const override { return false; }
+    bool isMidiEffect() const override { return false; }
     
-    const juce::String getName() const override final { return "DummyAudioProcessor"; }
+    const juce::String getName() const override { return "DummyAudioProcessor"; }
    
-    bool hasEditor() const override final { return false; }
-    juce::AudioProcessorEditor* createEditor() override final { return nullptr; }
+    bool hasEditor() const override { return false; }
+    juce::AudioProcessorEditor* createEditor() override { return nullptr; }
     
     /*=========================================================================================*/
     
