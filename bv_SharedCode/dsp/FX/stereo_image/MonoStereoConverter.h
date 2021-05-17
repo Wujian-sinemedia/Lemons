56ee919de9401504547d94a562f1ca2f13c7338c
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-17 01:11:17 -0700

    Update MonoStereoConverter.h

--- bv_SharedCode/dsp/FX/stereo_image/MonoStereoConverter.h
+++ bv_SharedCode/dsp/FX/stereo_image/MonoStereoConverter.h
@@ -1,1 +1,104 @@
 
+namespace bav::dsp::FX
+{
+
+template<typename SampleType>
+class MonoStereoConverter
+{
+    using Buffer = juce::AudioBuffer<SampleType>;
+    
+public:
+    MonoStereoConverter() = default;
+    
+    virtual ~MonoStereoConverter() = default;
+    
+    
+    enum StereoReductionMode
+    {
+        leftOnly,
+        rightOnly,
+        mixToMono
+    };
+    
+    
+    void prepare (int blocksize)
+    {
+        monoStorage.setSize (1, blocksize, true, true, true);
+    }
+    
+    
+    void setStereoReductionMode (StereoReductionMode newmode) { toMonoMode = newmode; }
+    
+    
+    void convertStereoToMono (const SampleType* leftIn, const SampleType* rightIn,
+                              SampleType* monoOut,
+                              int numSamples)
+    {
+        switch (toMonoMode)
+        {
+            case (leftOnly):
+            {
+                vecops::copy (leftIn, monoOut,  numSamples);
+            }
+            case (rightOnly):
+            {
+                vecops::copy (rightIn, monoOut,  numSamples);
+            }
+            case (mixToMono):
+            {
+                const auto pnt5 = static_cast<SampleType> (0.5);
+                monoStorage.copyFrom (0, 0, leftIn, numSamples, pnt5);
+                monoStorage.addFrom (0, 0, rightIn, numSamples, pnt5);
+                vecops::copy (monoStorage.getReadPointer(0), monoOut,  numSamples);
+            }
+        }
+    }
+    
+    void convertStereoToMono (const Buffer& input, Buffer& output)
+    {
+        jassert (input.getNumSamples() == output.getNumSamples());
+        
+        if (input.getNumChannels() == 1)
+        {
+            vecops::copy (input.getReadPointer(0), output.getWritePointer(0),  numSamples);
+        }
+        else
+        {
+            convertStereoToMono (input.getReadPointer(0), input.getReadPointer(1),
+                                 output.getWritePointer(0),
+                                 output.getNumSamples());
+        }
+    }
+    
+    
+    void convertMonoToStereo (const SampleType* monoIn,
+                              SampleType* leftOut, SampleType* rightOut,
+                              int numSamples)
+    {
+        vecops::copy (monoIn, leftOut,  numSamples);
+        vecops::copy (monoIn, rightOut, numSamples);
+    }
+    
+    void convertMonoToStereo (const Buffer& input, Buffer& output)
+    {
+        jassert (input.getNumSamples() == output.getNumSamples());
+        jassert (output.getNumChannels() >= 2);
+        
+        convertMonoToStereo (input.getReadPointer(0),
+                             output.getWritePointer(0), output.getWritePointer(1),
+                             output.getNumSamples());
+    }
+    
+    
+private:
+    MonoConversionMode toMonoMode;
+    
+    juce::AudioBuffer<SampleType> monoStorage;
+};
+
+
+template class MonoStereoConverter<float>;
+template class MonoStereoConverter<double>;
+
+
+}  // namespace
