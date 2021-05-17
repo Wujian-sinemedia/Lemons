59bdb8a27a8968f00445bf1a2a194b7e24e5e1d8
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-16 18:34:39 -0700

    added GrainExtractor to PSOLA analysis subdir

---
+++ bv_SharedCode/dsp/PSOLA/analysis/GrainExtractor/GrainExtractor.h
@@ -0,0 +1,73 @@
+
+#pragma once
+
+
+namespace bav
+{
+
+    
+template<typename SampleType>
+class GrainExtractor
+{
+    
+    using IArray = juce::Array<int>;
+    using FArray = juce::Array<float>;
+    
+    
+public:
+    
+    GrainExtractor();
+    
+    ~GrainExtractor();
+    
+    
+    void prepare (const int maxBlocksize);
+    
+    void releaseResources();
+    
+    
+    void getGrainOnsetIndices (IArray& targetArray, const SampleType* inputSamples, const int numSamples, const int period);
+    
+    
+private:
+    
+    IArray peakIndices; // used by all the kinds of peak picking algorithms to store their output for transformation to grains
+    
+    IArray peakCandidates;
+    IArray peakSearchingOrder;
+    
+    FArray candidateDeltas;
+    IArray   finalHandful;
+    FArray finalHandfulDeltas;
+    
+    // functions used for finding of PSOLA peaks
+    
+    void findPsolaPeaks (IArray& targetArray,
+                         const SampleType* reading,
+                         const int totalNumSamples,
+                         const int period);
+    
+    int findNextPeak (const int frameStart, const int frameEnd, int predictedPeak, const SampleType* reading, const IArray& targetArray,
+                      const int period, const int grainSize);
+    
+    void sortSampleIndicesForPeakSearching (IArray& output, const int startSample, const int endSample, const int predictedPeak);
+    
+    void getPeakCandidateInRange (IArray& candidates, const SampleType* input,
+                                  const int startSample, const int endSample, const int predictedPeak,
+                                  const IArray& searchingOrder);
+    
+    int chooseIdealPeakCandidate (const IArray& candidates, const SampleType* reading,
+                                  const int deltaTarget1, const int deltaTarget2);
+    
+    int choosePeakWithGreatestPower (const IArray& candidates, const SampleType* reading);
+    
+    //
+    
+    static constexpr auto numPeaksToTest          = 10;
+    static constexpr auto defaultFinalHandfulSize = 5;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GrainExtractor)
+};
+
+
+} // namespace
--- bv_SharedCode/dsp/PSOLA/analysis/psola_analyzer.cpp
+++ bv_SharedCode/dsp/PSOLA/analysis/psola_analyzer.cpp
@@ -1,1 +1,2 @@
 
+#include "GrainExtractor/GrainExtractor.cpp"
