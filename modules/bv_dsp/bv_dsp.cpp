616e3881d4a64ea633298f928c62a198212e8ce3
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-18 05:17:40 -0700

    updating

--- modules/bv_dsp/bv_dsp.cpp
+++ modules/bv_dsp/bv_dsp.cpp
@@ -1,7 +1,7 @@
 
-#include "dsp/FIFOs/AudioFIFO.cpp"
-#include "dsp/Oscillators/LFO/LFO.cpp"
-#include "dsp/FIFOWrappedEngine/FIFOWrappedEngine.cpp"
-#include "dsp/PitchDetector/pitch-detector.cpp"
-#include "dsp/PSOLA/analysis/psola_analyzer.cpp"
-#include "dsp/PSOLA/resynthesis/psola_shifter.cpp"
+#include "FIFOs/AudioFIFO.cpp"
+#include "Oscillators/LFO/LFO.cpp"
+#include "FIFOWrappedEngine/FIFOWrappedEngine.cpp"
+#include "PitchDetector/pitch-detector.cpp"
+#include "PSOLA/analysis/psola_analyzer.cpp"
+#include "PSOLA/resynthesis/psola_shifter.cpp"
