ea91400811bd3a07db13ed915814391fb0b905f4
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-21 19:10:13 -0700

    Update configureHelpers.cmake

--- CMake/internal/configureHelpers.cmake
+++ CMake/internal/configureHelpers.cmake
@@ -68,7 +68,7 @@ function (_configure_juce_app target useBrowser)
         bv_dsp
         bv_gui
         bv_midi
-        bv_mobile_utilities
+        bv_mobile_utils
         bv_core
         bv_SynthBase
         juce::juce_recommended_config_flags
