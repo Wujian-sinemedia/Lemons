d5d80c3903aaf7a538042841b714ea71855a8cf7
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-23 17:49:05 -0700

    updating mobile utils module

--- modules/bv_mobile_utils/bv_mobile_utils.h
+++ modules/bv_mobile_utils/bv_mobile_utils.h
@@ -14,112 +14,8 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-
-namespace bav
-{
-// to-do: rotation lock
-
-/*=====================================================================================================
- Classes dealing with screen rotation
- =====================================================================================================*/
-
-/* This struct listens for changes to a mobile device's orientation and recieves a callback when it changes.
- This class does nothing on desktop, and the callback will never be called. */
-
-struct DeviceRotationListener : private juce::Timer
-{
-    DeviceRotationListener (std::function< void() > callbackToUse);
-    virtual ~DeviceRotationListener();
-
-    void timerCallback() override final;
-
-private:
-    std::function< void() >           callback;
-    juce::Desktop::DisplayOrientation prevOrientation;
-};
-
-
-/* This struct allows you to lock the screen from rotating, or easily set landscape-only or vertical-only rotations. */
-
-struct DeviceRotation
-{
-    static void disableRotation();
-
-    static void enableOnlyVerticalRotations();
-    static void enableOnlyLandscapeRotations();
-};
-
-
-/*=====================================================================================================
- Control the auto-locking of the screen on mobile devices. On desktop, this disables the computer's screensaver.
- =====================================================================================================*/
-
-struct AutoLock
-{
-    static void setEnabled (bool shouldBeEnabled);
-};
-
-
-/*=====================================================================================================
- Classes dealing with motion & movement sensors
-=====================================================================================================*/
-
-struct MotionListener
-{
-    virtual ~MotionListener() = default;
-
-    virtual void accelerationChanged (double, double, double) { }
-    virtual void gravityChanged (double, double, double) { }
-    virtual void rotationChanged (double, double, double) { }
-    virtual void attitudeChanged (double, double, double) { }
-};
-
-
-class MotionManagerInterface
-{
-public:
-    struct Coords
-    {
-        double x = 0;
-        double y = 0;
-        double z = 0;
-    };
-
-    MotionManagerInterface()          = default;
-    virtual ~MotionManagerInterface() = default;
-
-    virtual void start()     = 0;
-    virtual void stop()      = 0;
-    virtual bool isRunning() = 0;
-
-    Coords getAcceleration() const { return acceleration; }
-    Coords getGravity() const { return gravity; }
-    Coords getRotation() const { return rotation; }
-    Coords getAttitude() const { return attitude; }
-
-    void addListener (MotionListener* l) { listeners.add (l); }
-    void removeListener (MotionListener* l) { listeners.remove (l); }
-
-protected:
-    void accelerationChanged (double x, double y, double z);
-    void gravityChanged (double x, double y, double z);
-    void rotationChanged (double x, double y, double z);
-    void attitudeChanged (double x, double y, double z);
-
-    Coords acceleration;
-    Coords gravity;
-    Coords rotation;
-    Coords attitude;
-
-    juce::ListenerList< MotionListener > listeners;
-};
-
-
-}  // namespace bav
-
-/*=====================================================================================================
- =====================================================================================================*/
-
+#include "all_platforms/motion.h"
+#include "all_platforms/rotation.h"
 
 #if JUCE_IOS
 #    include "iOS/iosMotion.h"
@@ -131,3 +27,18 @@ protected:
 #    include "dummy_desktop_versions/DummyMotionManager.h"
 
 #endif
+
+
+namespace bav
+{
+/*=====================================================================================================
+ Control the auto-locking of the screen on mobile devices. On desktop, this disables the computer's screensaver.
+ =====================================================================================================*/
+
+struct AutoLock
+{
+    static void setEnabled (bool shouldBeEnabled);
+};
+
+
+}  // namespace bav
