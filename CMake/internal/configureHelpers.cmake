2cef18b1f4990fbd177df754fb3a781effff0351
Ben Vining <ben.the.vining@gmail.com>
2021-05-30 00:12:18 -0700

    updating

--- CMake/internal/configureHelpers.cmake
+++ CMake/internal/configureHelpers.cmake
@@ -23,31 +23,8 @@ function (_configure_vecops_options target)
     if (APPLE AND NOT ${BV_IGNORE_VDSP})
         message (STATUS "Configuring vDSP for vecops...")
         target_compile_definitions (${target} PUBLIC JUCE_USE_VDSP_FRAMEWORK=1 BV_USE_VDSP=1)
-        return()
-    endif()
-
-    if (NOT DEFINED BV_IGNORE_MIPP)
-        set (BV_IGNORE_MIPP FALSE)
-    endif()
-
-    if (NOT ${BV_IGNORE_MIPP})
-        message (STATUS "Configuring MIPP for vecops...")
-        target_compile_definitions (${target} PUBLIC MIPP_ENABLE_BACKTRACE BV_USE_MIPP=1)
-        target_include_directories (${target} PUBLIC "${bv_sharedcode_dir}/third_party/MIPP/src" "MIPP")
-    endif()
-endfunction()
-
-#
-
-function (_configure_mts_esp target)
-    if (NOT DEFINED BV_USE_MTS_ESP)
-        set (BV_USE_MTS_ESP FALSE)
-    endif()
-
-    if (${BV_USE_MTS_ESP})
-        message (STATUS "Configuring MTS-ESP...")
-        target_include_directories (${target} PUBLIC "${bv_sharedcode_dir}/third_party/MTS-ESP/Client" "MTS-ESP")
-        target_compile_definitions (${target} PUBLIC BV_USE_MTS_ESP=1)
+    else()
+        _configure_mipp (${target})
     endif()
 endfunction()
 
