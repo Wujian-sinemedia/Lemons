af1ed837def2b8f10d398989a23f77345c39b721
Ben Vining <ben.the.vining@gmail.com>
2021-05-29 18:23:48 -0700

    updating organization of third party submodules

--- CMake/publicAPI.cmake
+++ CMake/publicAPI.cmake
@@ -10,6 +10,7 @@ function (link_binary_data_to_target_create_if_needed target folder)
     _add_resources_folder_dont_regenerate (${target} ${CMAKE_CURRENT_LIST_DIR}/${folder})
 endfunction()
 
+
 ###########
 
 function (configure_default_juce_plugin target)
