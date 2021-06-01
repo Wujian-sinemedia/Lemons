cf3da92736600d201a5cf0e0abd8793280bc5a84
Ben Vining <ben.the.vining@gmail.com>
2021-05-31 22:47:02 -0700

    added docs dir

--- shell_scripts/update_git_submodules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull
-
-git submodule foreach 'git fetch && git checkout origin/HEAD && git submodule update --recursive --merge'
-
-git commit -m "submodule auto-update" -a
-
-git push
