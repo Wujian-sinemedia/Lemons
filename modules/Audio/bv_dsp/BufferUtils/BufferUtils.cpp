76b541ef5dd3b3d45706a0f63d4dd2179d0d6778
Ben Vining <ben.the.vining@gmail.com>
2021-07-02 03:11:01 -0700

    updating

--- modules/Audio/bv_dsp/BufferUtils/BufferUtils.cpp
+++ modules/Audio/bv_dsp/BufferUtils/BufferUtils.cpp
@@ -19,4 +19,20 @@ void copy (const juce::AudioBuffer<SampleType>& source, juce::AudioBuffer<Sample
 template void copy (const juce::AudioBuffer<float>&, juce::AudioBuffer<float>&);
 template void copy (const juce::AudioBuffer<double>&, juce::AudioBuffer<double>&);
 
+template<typename Type1, typename Type2>
+void convert (const juce::AudioBuffer<Type1>& source, juce::AudioBuffer<Type2>& dest)
+{
+    dest.clear();
+    
+    const auto numSamples = source.getNumSamples();
+    jassert (dest.getNumSamples() >= numSamples);
+    
+    const auto numChannels = std::min (source.getNumChannels(), dest.getNumChannels());
+    
+    for (int chan = 0; chan < numChannels; ++chan)
+        vecops::convert (dest.getWritePointer(chan), source.getReadPointer(chan), numSamples);
+}
+template void convert (const juce::AudioBuffer<float>&, juce::AudioBuffer<double>&);
+template void convert (const juce::AudioBuffer<double>&, juce::AudioBuffer<float>&);
+
 }
