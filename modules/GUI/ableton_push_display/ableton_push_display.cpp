a09f728569421db2cd598d055e363f6746b35141
Ben Vining <ben.the.vining@gmail.com>
2021-07-06 20:09:31 -0700

    reorganizing

--- modules/GUI/ableton_push_display/ableton_push_display.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "ableton_push_display.h"
-
-#include "usb_communication/push_usb.cpp"
-#include "display_bridge/push_display_bridge.cpp"
--- modules/GUI/ableton_push_display/usb_communication/push_usb.cpp
+++
@@ -1,5 +0,0 @@
-
-namespace ableton
-{
-
-}  // namespace
