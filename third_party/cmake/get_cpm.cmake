d2a663db5b1af6f0b9e93c0258ff4800190bea6a
Ben Vining <ben.the.vining@gmail.com>
2021-07-11 00:25:27 -0700

    removed juce as a submodule

---
+++ third_party/cmake/get_cpm.cmake
@@ -0,0 +1,15 @@
+if (NOT DEFINED CPM_DOWNLOAD_LOCATION)
+  set (CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM.cmake")
+endif()
+
+if (NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
+
+  message (STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
+  
+  file (DOWNLOAD
+       https://raw.githubusercontent.com/cpm-cmake/CPM.cmake/master/cmake/CPM.cmake
+       ${CPM_DOWNLOAD_LOCATION})
+
+endif()
+
+include (${CPM_DOWNLOAD_LOCATION})
