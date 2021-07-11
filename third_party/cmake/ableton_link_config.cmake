c6216e257f23ed5183b36fa315c8176554c4c633
Ben Vining <ben.the.vining@gmail.com>
2021-07-11 00:37:55 -0700

    removed other submodules

--- third_party/cmake/ableton_link_config.cmake
+++ third_party/cmake/ableton_link_config.cmake
@@ -8,15 +8,13 @@ macro (_configure_ableton_link)
 
 	    message (STATUS "Configuring Ableton Link...")
 
-	    set (_ABLETON_LINK_DIR ${BV_THIRD_PARTY_DIR}/ableton-link)
-
-	    include (${_ABLETON_LINK_DIR}/AbletonLinkConfig.cmake)
+	    include (${bv_ableton_link_dir}/AbletonLinkConfig.cmake)
 
 	    target_compile_definitions (Ableton::Link INTERFACE BV_USE_ABLETON_LINK=1)
 
 	    target_include_directories (Ableton::Link INTERFACE 
-	        "${_ABLETON_LINK_DIR}/include/ableton"
-	        "${_ABLETON_LINK_DIR}/include/ableton/platforms")
+	        "${bv_ableton_link_dir}/include/ableton"
+	        "${bv_ableton_link_dir}/include/ableton/platforms")
 
 	    target_link_libraries (bv_plugin INTERFACE Ableton::Link)
 
