d8a08016fae47135e7a831a4676992b65d56c770
Ben Vining <ben.the.vining@gmail.com>
2021-07-11 01:43:02 -0700

    Update publicAPI.cmake

--- CMake/publicAPI.cmake
+++ CMake/publicAPI.cmake
@@ -19,3 +19,20 @@ function (configure_default_juce_app target)
     _configure_juce_app (${target} TRUE)
 endfunction()
 
+
+###########
+
+
+macro (bv_detect_default_plugin_formats outvar)
+    if ("${CMAKE_SYSTEM_NAME}" STREQUAL "iOS")
+        set (${outvar} Standalone)
+    elseif (APPLE)
+        set (${outvar} Standalone Unity VST3 AU AUv3)
+    elseif (WIN32)
+        set (${outvar} Standalone Unity VST3)
+    else()
+
+        set (${outvar} Standalone Unity VST3)
+
+    endif()
+endmacro()
