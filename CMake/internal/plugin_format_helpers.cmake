1a60d20881d6ec683f0a690019fbf963f85651fb
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 04:27:01 -0700

    Update plugin_format_helpers.cmake

--- CMake/internal/plugin_format_helpers.cmake
+++ CMake/internal/plugin_format_helpers.cmake
@@ -1,8 +1,21 @@
+if (NOT APPLE AND NOT WIN32 AND EXISTS ${JUCE_SOURCE_DIR}/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.cpp)
+	set (BV_LV2_AVAILABLE TRUE CACHE INTERNAL "Availability of LV2 plugin format")
+	message (STATUS "LV2 plugin format capability detected :-D")
+else()
+	set (BV_LV2_AVAILABLE FALSE CACHE INTERNAL "Availability of LV2 plugin format")
+endif()
+
+#
+
 function (_bv_parse_plugin_format_list outlist)
 
-	set (formatlist "")
+	set (valid_formats Standalone Unity VST3 AU AUv3)
 
-	set (valid_formats Standalone Unity VST3 AU AUv3 LV2)
+	if (${BV_LV2_AVAILABLE})
+		list (APPEND valid_formats LV2)
+	endif()
+
+	set (formatlist "")
 
 	foreach (format ${Formats})
 		if ("${format}" IN_LIST valid_formats)
@@ -14,6 +27,7 @@ function (_bv_parse_plugin_format_list outlist)
 
 endfunction()
 
+#
 
 function (_bv_make_plugin_format_list outvar)
 
@@ -33,12 +47,10 @@ function (_bv_make_plugin_format_list outvar)
         set (formatlist Standalone)
     elseif (APPLE)
         set (formatlist Standalone Unity VST3 AU)  # TODO: add AUv3
-    elseif (WIN32)
-        set (formatlist Standalone Unity VST3)
+    elseif (${BV_LV2_AVAILABLE})
+		set (formatlist Standalone Unity VST3 LV2)
     else()
-
         set (formatlist Standalone Unity VST3)
-
     endif()
 
     message (STATUS "Using default format list for platform: ${formatlist}")
