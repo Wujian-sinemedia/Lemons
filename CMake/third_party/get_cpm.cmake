defe51c700267cd553333fb115ee1bd4c0bc829a
Ben Vining <ben.the.vining@gmail.com>
2021-07-12 23:51:00 -0700

    Update get_cpm.cmake

--- CMake/third_party/get_cpm.cmake
+++ CMake/third_party/get_cpm.cmake
@@ -1,13 +1,23 @@
-macro (download_cpm)
-  set (BV_CPM_PATH ${CMAKE_CURRENT_LIST_DIR}/CPM.cmake CACHE INTERNAL "Path to the CPM.cmake script")
+macro (_bv_download_cpm)
 
-  message (STATUS "Downloading CPM.cmake to ${BV_CPM_PATH}")
+  if (DEFINED ENV{CPM_SOURCE_CACHE})
+    set (BV_CPM_PATH $ENV{CPM_SOURCE_CACHE}/CPM.cmake CACHE INTERNAL "Path to the CPM.cmake script")
+  else()
+    set (BV_CPM_PATH ${CMAKE_CURRENT_LIST_DIR}/CPM.cmake CACHE INTERNAL "Path to the CPM.cmake script")
+  endif()
+
+  if (NOT EXISTS ${BV_CPM_PATH})
+
+    message (STATUS "Downloading CPM.cmake to ${BV_CPM_PATH}")
   
-  file (DOWNLOAD
-       https://raw.githubusercontent.com/cpm-cmake/CPM.cmake/master/cmake/CPM.cmake
-       ${BV_CPM_PATH})
+    file (DOWNLOAD
+        https://raw.githubusercontent.com/cpm-cmake/CPM.cmake/master/cmake/CPM.cmake
+        ${BV_CPM_PATH})
+
+  endif()
 
   include (${BV_CPM_PATH})
+
 endmacro()
 
 #
@@ -16,8 +26,8 @@ if (DEFINED BV_CPM_PATH)
   if (EXISTS ${BV_CPM_PATH})
     include (${BV_CPM_PATH})
   else()
-    download_cpm()
+    _bv_download_cpm()
   endif()
 else()
-  download_cpm()
+  _bv_download_cpm()
 endif()
