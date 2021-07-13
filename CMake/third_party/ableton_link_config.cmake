82da5bf6d6efc763e733a24485ef819d23910dc6
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 00:07:12 -0700

    updating

--- CMake/third_party/ableton_link_config.cmake
+++ CMake/third_party/ableton_link_config.cmake
@@ -5,28 +5,28 @@ CPMAddPackage (
 
 set (bv_ableton_link_dir ${AbletonLink_SOURCE_DIR} CACHE INTERNAL "Path to Ableton Link sources")
 
+if (NOT DEFINED BV_USE_ABLETON_LINK)
+    set (BV_USE_ABLETON_LINK FALSE)
+endif()
+
 #
 
-macro (_configure_ableton_link)
+function (_configure_ableton_link)
 
-	if (NOT DEFINED BV_USE_ABLETON_LINK)
-		set (BV_USE_ABLETON_LINK FALSE)
-	endif()
+    if (NOT ${BV_USE_ABLETON_LINK})
+	return()
+    endif()
 
-	if (${BV_USE_ABLETON_LINK})
+    message (STATUS "Configuring Ableton Link...")
 
-	    message (STATUS "Configuring Ableton Link...")
+    include (${bv_ableton_link_dir}/AbletonLinkConfig.cmake)
 
-	    include (${bv_ableton_link_dir}/AbletonLinkConfig.cmake)
+    target_compile_definitions (Ableton::Link INTERFACE BV_USE_ABLETON_LINK=1)
 
-	    target_compile_definitions (Ableton::Link INTERFACE BV_USE_ABLETON_LINK=1)
+    target_include_directories (Ableton::Link INTERFACE 
+        "${bv_ableton_link_dir}/include/ableton"
+        "${bv_ableton_link_dir}/include/ableton/platforms")
 
-	    target_include_directories (Ableton::Link INTERFACE 
-	        "${bv_ableton_link_dir}/include/ableton"
-	        "${bv_ableton_link_dir}/include/ableton/platforms")
+    target_link_libraries (bv_plugin INTERFACE Ableton::Link)
 
-	    target_link_libraries (bv_plugin INTERFACE Ableton::Link)
-
-	endif()
-
-endmacro()
+endfunction()
--- CMake/third_party/mts_esp_config.cmake
+++ CMake/third_party/mts_esp_config.cmake
@@ -1,4 +1,3 @@
-
 CPMAddPackage (
         NAME MTS-ESP
         GIT_REPOSITORY https://github.com/ODDSound/MTS-ESP.git
@@ -6,30 +5,30 @@ CPMAddPackage (
 
 set (bv_mts_esp_dir ${MTS-ESP_SOURCE_DIR}/Client CACHE INTERNAL "Path to MTS-ESP sources")
 
+if (NOT DEFINED BV_USE_MTS_ESP)
+    set (BV_USE_MTS_ESP FALSE)
+endif()
+
 #
 
-macro (_configure_mts_esp)
+function (_configure_mts_esp)
 
-	if (NOT DEFINED BV_USE_MTS_ESP)
-	    set (BV_USE_MTS_ESP FALSE)
-	endif()
+    if (NOT ${BV_USE_MTS_ESP})
+	return()
+    endif()
 
-	if (${BV_USE_MTS_ESP})
+    message (STATUS "Configuring MTS-ESP...")
 
-		message (STATUS "Configuring MTS-ESP...")
+    add_library (MTS-ESP INTERFACE)
 
-		add_library (MTS-ESP INTERFACE)
+    set (MTS_ESP_SOURCES "${bv_mts_esp_dir}/libMTSClient.cpp")
 
-	    set (MTS_ESP_SOURCES "${bv_mts_esp_dir}/libMTSClient.cpp")
+    target_sources             (MTS-ESP INTERFACE ${MTS_ESP_SOURCES})
+    target_include_directories (MTS-ESP INTERFACE ${bv_mts_esp_dir})
+    target_compile_definitions (MTS-ESP INTERFACE BV_USE_MTS_ESP=1)
 
-	    target_sources             (MTS-ESP INTERFACE ${MTS_ESP_SOURCES})
-	    target_include_directories (MTS-ESP INTERFACE ${bv_mts_esp_dir})
-	    target_compile_definitions (MTS-ESP INTERFACE BV_USE_MTS_ESP=1)
+    set_source_files_properties (${MTS_ESP_SOURCES} TARGET_DIRECTORY bv_midi PROPERTIES COMPILE_FLAGS "-w")
 
-	    set_source_files_properties (${MTS_ESP_SOURCES} TARGET_DIRECTORY bv_midi PROPERTIES COMPILE_FLAGS "-w")
+    target_link_libraries (bv_midi INTERFACE MTS-ESP)
 
-	    target_link_libraries (bv_midi INTERFACE MTS-ESP)
-
-	endif()
-
-endmacro()
+endfunction()
