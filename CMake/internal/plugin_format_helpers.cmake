3fb3aefadea6268199d00bd3c4710eb9859d1654
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 03:56:24 -0700

    updating

---
+++ CMake/internal/plugin_format_helpers.cmake
@@ -0,0 +1,47 @@
+function (_bv_parse_plugin_format_list outlist)
+
+	set (formatlist "")
+
+	set (valid_formats Standalone Unity VST3 AU AUv3)
+
+	foreach (format ${Formats})
+		if ("${format}" IN_LIST valid_formats)
+			list (APPEND formatlist ${format})
+		endif()
+	endforeach()
+
+	set (${outlist} ${formatlist} PARENT_SCOPE)
+
+endfunction()
+
+
+function (_bv_make_plugin_format_list outvar)
+
+	set (formatlist "")
+
+	if (DEFINED Formats)
+		_bv_parse_plugin_format_list (formatlist)
+	endif()
+
+	if (NOT "${formatlist}" STREQUAL "")
+		message (STATUS "Format list successfully parsed from command line: ${formatlist}")
+		set (${outvar} ${formatlist} PARENT_SCOPE)
+		return()
+	endif()
+
+	if ("${CMAKE_SYSTEM_NAME}" STREQUAL "iOS")
+        set (formatlist Standalone)
+    elseif (APPLE)
+        set (formatlist Standalone Unity VST3 AU)  # TODO: add AUv3
+    elseif (WIN32)
+        set (formatlist Standalone Unity VST3)
+    else()
+
+        set (formatlist Standalone Unity VST3)
+
+    endif()
+
+    message (STATUS "Using default format list for platform: ${formatlist}")
+    set (${outvar} ${formatlist} PARENT_SCOPE)
+
+endfunction()
--- CMake/publicAPI.cmake
+++ CMake/publicAPI.cmake
@@ -1,6 +1,5 @@
 
 function (add_binary_data_folder target folder)
-    
     _add_resources_folder (${target} ${CMAKE_CURRENT_LIST_DIR}/${folder})
 endfunction()
 
@@ -9,13 +8,11 @@ endfunction()
 
 
 function (configure_default_juce_plugin target)
-
 	_configure_juce_app (${target} TRUE)
 endfunction()
 
 
 function (configure_default_juce_app target)
-
     _configure_juce_app (${target} TRUE)
 endfunction()
 
@@ -24,15 +21,5 @@ endfunction()
 
 
 macro (bv_detect_default_plugin_formats outvar)
-    if ("${CMAKE_SYSTEM_NAME}" STREQUAL "iOS")
-        set (${outvar} Standalone)
-    elseif (APPLE)
-        set (${outvar} Standalone Unity VST3 AU)
-    elseif (WIN32)
-        set (${outvar} Standalone Unity VST3)
-    else()
-
-        set (${outvar} Standalone Unity VST3)
-
-    endif()
+    _bv_make_plugin_format_list (${outvar})
 endmacro()
