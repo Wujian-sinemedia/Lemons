97a5479aaea892212230bebfc9bc1a8ca6b2152c
Ben Vining <ben.the.vining@gmail.com>
2021-07-14 19:55:38 -0700

    created a brand flags file

---
+++ cmake/internal/BrandSpecific.cmake
@@ -0,0 +1,14 @@
+#[[
+	This file sets some flags that are specific to my brand's plugins and apps.
+	Author: Ben Vining
+]]
+
+
+set (BV_DEFAULT_BRAND_FLAGS
+	BUNDLE_ID                   com.BenViningMusicSoftware.${CMAKE_PROJECT_NAME}
+	COMPANY_NAME                BenViningMusicSoftware
+    COMPANY_WEBSITE             www.benvining.com
+    COMPANY_EMAIL               ben.the.vining@gmail.com
+    COMPANY_COPYRIGHT           "Copyright 2021 Ben Vining"
+    PLUGIN_MANUFACTURER_CODE	Benv
+	PARENT_SCOPE)
