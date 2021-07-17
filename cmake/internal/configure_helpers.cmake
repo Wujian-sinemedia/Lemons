9bcc498dba5872e6b975954ccdf89e68fddec753
Ben Vining <ben.the.vining@gmail.com>
2021-07-17 05:17:16 -0700

    reorganizing cmake

--- cmake/internal/configure_helpers.cmake
+++ cmake/internal/configure_helpers.cmake
@@ -111,3 +111,18 @@ function (_bv_finish_app_config target)
 
 endfunction()
 
+#
+
+function (_bv_add_resources_folder target folder)
+    set (resourcesTarget ${target}-Assets)
+
+    if (NOT TARGET ${resourcesTarget})
+        file (GLOB_RECURSE files "${folder}/*.*")
+        juce_add_binary_data (${resourcesTarget} SOURCES ${files})
+        set_target_properties (${resourcesTarget} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+        target_compile_definitions (${resourcesTarget} INTERFACE BV_HAS_BINARY_DATA=1)
+    endif()
+
+    target_link_libraries (${target} PRIVATE ${resourcesTarget})
+endfunction()
+
