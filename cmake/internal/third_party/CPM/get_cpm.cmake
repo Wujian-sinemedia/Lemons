d853e7fe9a0b89e9974fec4e4acbf5a4c5696e85
Ben Vining <ben.the.vining@gmail.com>
2021-07-19 00:06:24 -0700

    Update get_cpm.cmake

--- cmake/internal/third_party/CPM/get_cpm.cmake
+++ cmake/internal/third_party/CPM/get_cpm.cmake
@@ -5,6 +5,15 @@ if (DEFINED CACHE{BV_CPM_PATH})
     endif()
 endif()
 
+# Using the local version - update it first!
+message (STATUS "Updating committed version of CPM.cmake...")
+    
+file (REMOVE ${SHAREDCODE_LOCAL_CPM_PATH})
+
+file (DOWNLOAD
+    https://raw.githubusercontent.com/cpm-cmake/CPM.cmake/master/cmake/CPM.cmake
+    ${SHAREDCODE_LOCAL_CPM_PATH})
+
 set (BV_CPM_PATH ${SHAREDCODE_LOCAL_CPM_PATH} CACHE INTERNAL "Path to the CPM.cmake script")
 
 include ($CACHE{BV_CPM_PATH})
