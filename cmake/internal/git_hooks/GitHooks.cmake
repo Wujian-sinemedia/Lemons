0fcccde07c12289704b57313ea2f961ec93d8922
Ben Vining <ben.the.vining@gmail.com>
2021-07-18 21:10:11 -0700

    Update GitHooks.cmake

--- cmake/internal/git_hooks/GitHooks.cmake
+++ cmake/internal/git_hooks/GitHooks.cmake
@@ -1,19 +1,19 @@
 set (BV_GITHOOKS_DIR ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "Path to githook scripts")
 
-function (bv_configure_precommit_git_hook projectDir)
-    if (NOT EXISTS ${projectDir}/.git)
-        return()
-    endif()
+if (NOT GIT_FOUND)
+    find_package (Git QUIET)
+endif()
 
-    if (NOT GIT_FOUND)
-        find_package (Git QUIET)
+function (bv_configure_precommit_git_hook projectDir)
+    if (NOT GIT_FOUND OR NOT EXISTS ${projectDir}/.git)
+        return()
     endif()
 
     if (NOT CLANG_FORMAT)
         find_program (CLANG_FORMAT clang-format)
     endif()
 
-    if (NOT GIT_FOUND OR NOT CLANG_FORMAT)
+    if (NOT CLANG_FORMAT)
         return()
     endif()
 
