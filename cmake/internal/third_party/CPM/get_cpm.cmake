ca67705b49893c1718c93a99f8c71090c46bfb6c
Ben Vining <ben.the.vining@gmail.com>
2021-07-18 23:54:15 -0700

    Update get_cpm.cmake

--- cmake/internal/third_party/CPM/get_cpm.cmake
+++ cmake/internal/third_party/CPM/get_cpm.cmake
@@ -1,10 +1,10 @@
-if (DEFINED BV_CPM_PATH)
-    if (EXISTS ${BV_CPM_PATH})
-        include (${BV_CPM_PATH})
+if (DEFINED CACHE{BV_CPM_PATH})
+    if (EXISTS $CACHE{BV_CPM_PATH})
+        include ($CACHE{BV_CPM_PATH})
         return()
     endif()
 endif()
 
 set (BV_CPM_PATH ${SHAREDCODE_LOCAL_CPM_PATH} CACHE INTERNAL "Path to the CPM.cmake script")
 
-include (${BV_CPM_PATH})
+include ($CACHE{BV_CPM_PATH})
