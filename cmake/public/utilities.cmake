cb6e132e83c8db3b0d2e5d64a9032d7b61113c01
Ben Vining <ben.the.vining@gmail.com>
2021-07-19 01:11:31 -0700

    Update utilities.cmake

--- cmake/public/utilities.cmake
+++ cmake/public/utilities.cmake
@@ -27,3 +27,21 @@ function (bv_list_all_cmake_variables)
   	endforeach()
   	message (STATUS "")
 endfunction()
+
+
+function (bv_apply_git_patch gitdir patch_path)
+	if (NOT GIT_FOUND)
+    	find_package (Git QUIET)
+	endif()
+
+	if (NOT GIT_FOUND)
+		message (WARNING "Git not found!")
+		return()
+	endif()
+
+	execute_process (COMMAND ${GIT_EXECUTABLE} reset --hard HEAD
+            		 WORKING_DIRECTORY ${gitdir})
+
+    execute_process (COMMAND ${GIT_EXECUTABLE} apply ${patch_path}
+            		 WORKING_DIRECTORY ${gitdir})
+endfucntion()
