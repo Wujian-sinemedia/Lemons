be3b94f6e5709b65c94445afe4bbbdfa789fda64
Ben Vining <ben.the.vining@gmail.com>
2021-07-20 03:15:17 -0700

    Update public_api.cmake

--- cmake/public/public_api.cmake
+++ cmake/public/public_api.cmake
@@ -30,54 +30,42 @@ endfunction()
 
 #
 
-function (bv_configure_juce_plugin)
+macro (_bv_juce_target_internal_config)
 
     set (options BROWSER MTS-ESP ALWAYS_VDSP NEVER_VDSP)
     set (oneValueArgs TARGET)
     set (multiValueArgs "")
 
-    cmake_parse_arguments (BV_PLUGINTARGETCONFIG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments (BV_TARGETCONFIG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    if (NOT DEFINED BV_PLUGINTARGETCONFIG_TARGET)
-        bv_print_warning ("Target name not specified in call to bv_configure_juce_plugin")
+    if (NOT DEFINED BV_TARGETCONFIG_TARGET)
+        bv_print_warning ("Target name not specified in call to _bv_juce_target_internal_config")
         return()
     endif()
 
-	_bv_configure_juce_target (${BV_PLUGINTARGETCONFIG_TARGET} 
-                               ${BV_PLUGINTARGETCONFIG_BROWSER}
-                               ${BV_PLUGINTARGETCONFIG_MTS-ESP}
-                               ${BV_PLUGINTARGETCONFIG_ALWAYS_VDSP}
-                               ${BV_PLUGINTARGETCONFIG_NEVER_VDSP})
+    _bv_configure_juce_target (${BV_TARGETCONFIG_TARGET} 
+                               ${BV_TARGETCONFIG_BROWSER}
+                               ${BV_TARGETCONFIG_MTS-ESP}
+                               ${BV_TARGETCONFIG_ALWAYS_VDSP}
+                               ${BV_TARGETCONFIG_NEVER_VDSP})
+endmacro()
 
-    target_link_libraries (${BV_PLUGINTARGETCONFIG_TARGET} PUBLIC ${BV_PLUGIN_ONLY_MODULES})
 
-    target_compile_definitions (${BV_PLUGINTARGETCONFIG_TARGET} PUBLIC
+function (bv_configure_juce_plugin)
+    _bv_juce_target_internal_config (${ARGN})
+
+    target_link_libraries (${BV_TARGETCONFIG_TARGET} PUBLIC ${BV_PLUGIN_ONLY_MODULES})
+
+    target_compile_definitions (${BV_TARGETCONFIG_TARGET} PUBLIC
             JUCE_MICROPHONE_PERMISSION_ENABLED=1
             JUCE_USE_CUSTOM_PLUGIN_STANDALONE_APP=0)
 endfunction()
 
-#
 
 function (bv_configure_juce_app)
+    _bv_juce_target_internal_config (${ARGN})
 
-    set (options BROWSER MTS-ESP ALWAYS_VDSP NEVER_VDSP)
-    set (oneValueArgs TARGET)
-    set (multiValueArgs "")
-
-    cmake_parse_arguments (BV_APPTARGETCONFIG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    if (NOT DEFINED BV_APPTARGETCONFIG_TARGET)
-        bv_print_warning ("Target name not specified in call to bv_configure_juce_app")
-        return()
-    endif()
-
-    _bv_configure_juce_target (${BV_APPTARGETCONFIG_TARGET} 
-                               ${BV_APPTARGETCONFIG_BROWSER}
-                               ${BV_APPTARGETCONFIG_MTS-ESP}
-                               ${BV_APPTARGETCONFIG_ALWAYS_VDSP}
-                               ${BV_APPTARGETCONFIG_NEVER_VDSP})
-
-    target_link_libraries (${BV_APPTARGETCONFIG_TARGET} PUBLIC ${BV_APP_ONLY_MODULES})
+    target_link_libraries (${BV_TARGETCONFIG_TARGET} PUBLIC ${BV_APP_ONLY_MODULES})
 endfunction()
 
 
