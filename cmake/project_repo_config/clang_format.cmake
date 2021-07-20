4bf56f014c90cc11c897adc8f1488e129d25b848
Ben Vining <ben.the.vining@gmail.com>
2021-07-19 19:46:26 -0700

    updating

--- cmake/project_repo_config/clang_format.cmake
+++ cmake/project_repo_config/clang_format.cmake
@@ -1,47 +1,27 @@
 set (BV_CLANG_FORMAT_FILETYPES "*.h *.hpp *.c *.cpp" CACHE INTERNAL "Filetypes clang-format will be used on")
 
-macro (_bv_update_repo_clangformat_config_file _repo_dir)
-    bv_copy_file (${Shared-code_SOURCE_DIR}/_clang-format ${_repo_dir}/_clang-format)
-endmacro()
-
 if (NOT CLANG_FORMAT)
     find_program (CLANG_FORMAT clang-format)
 endif()
 
-if (NOT CLANG_FORMAT)
-    function (bv_update_repo_clangformat repodir)
-        _bv_update_repo_clangformat_config_file (${repodir})
-    endfunction()
 
-    function (_bv_update_clangformat_recursive_script sourcedir repodir)
-    endfunction()
+macro (_bv_update_repo_clangformat_config_file _repo_dir)
+    bv_copy_file (${Shared-code_SOURCE_DIR}/_clang-format ${_repo_dir}/_clang-format)
+endmacro()
 
-    function (bv_update_repo_clangformat repodir)
-        _bv_update_repo_clangformat_config_file (${repodir})
-        _bv_update_clangformat_recursive_script (${repodir}/Source ${repodir})
-    endfunction()
-
-    return()
-endif()
-
-#
 
 function (_bv_update_clangformat_recursive_script sourcedir repodir)
-    if (NOT UNIX)
-        return()
-    endif()
-
-    set (BV_PROJECT_SOURCE_DIR ${sourcedir})
-
-    if (NOT EXISTS ${BV_PROJECT_SOURCE_DIR})
-        return()
-    endif()
-
     set (DEST_DIR ${repodir}/run_clang_format.sh)
 
     if (EXISTS ${DEST_DIR})
         file (REMOVE ${DEST_DIR})
     endif()
+
+    if (NOT UNIX OR NOT CLANG_FORMAT OR NOT EXISTS ${sourcedir})
+        return()
+    endif()
+
+    set (BV_PROJECT_SOURCE_DIR ${sourcedir})
 
     configure_file (${BV_REPO_CONFIG_FILES_DIR}/run_clang_format.sh ${DEST_DIR})
 
--- cmake/project_repo_config/git_hooks/GitHooks.cmake
+++ cmake/project_repo_config/git_hooks/GitHooks.cmake
@@ -4,27 +4,16 @@ if (NOT GIT_FOUND)
     find_package (Git QUIET)
 endif()
 
-if (NOT GIT_FOUND OR NOT UNIX)
-    function (bv_configure_precommit_git_hook projectDir)
-    endfunction()
-
-    return()
-endif()
-
 
 function (bv_configure_precommit_git_hook projectDir)
-    if (NOT EXISTS ${projectDir}/.git)
-        return()
-    endif()
-
-    if (NOT CLANG_FORMAT)
-        return()
-    endif()
-
     set (DEST_DIR ${projectDir}/.git/hooks/pre-commit)
     
     if (EXISTS ${DEST_DIR})
         file (REMOVE ${DEST_DIR})
+    endif()
+
+    if (NOT GIT_FOUND OR NOT CLANG_FORMAT OR NOT UNIX OR NOT EXISTS ${projectDir}/.git)
+        return()
     endif()
 
     configure_file (${BV_GITHOOKS_DIR}/pre_commit_script.in
