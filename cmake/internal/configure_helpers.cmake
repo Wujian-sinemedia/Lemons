75fc28880c424acef3a7e18d14c409d559bdc027
Ben Vining <ben.the.vining@gmail.com>
2021-07-20 03:22:14 -0700

    updating

--- cmake/internal/configure_helpers.cmake
+++ cmake/internal/configure_helpers.cmake
@@ -58,17 +58,28 @@ endfunction()
 
 #
 
-function (_bv_configure_juce_target target useBrowser use_MTS-ESP forceUseVDSP forceIgnoreVDSP)
-    if (TARGET ${target}_AAX)
-        set_target_properties (${target}_AAX PROPERTIES OSX_ARCHITECTURES x86_64)
+function (_bv_configure_juce_target)
+
+    set (options BROWSER MTS-ESP ALWAYS_VDSP NEVER_VDSP)
+    set (oneValueArgs TARGET)
+    set (multiValueArgs "")
+
+    cmake_parse_arguments (BV_TARGETCONFIG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if (NOT DEFINED BV_TARGETCONFIG_TARGET)
+        message (FATAL_ERROR "Target name not specified in call to _bv_configure_juce_target")
     endif()
 
-    _bv_configure_LV2_URI (${target})
+    if (TARGET ${BV_TARGETCONFIG_TARGET}_AAX)
+        set_target_properties (${BV_TARGETCONFIG_TARGET}_AAX PROPERTIES OSX_ARCHITECTURES x86_64)
+    endif()
 
-    target_compile_definitions (${target} PUBLIC
+    _bv_configure_LV2_URI (${BV_TARGETCONFIG_TARGET})
+
+    target_compile_definitions (${BV_TARGETCONFIG_TARGET} PUBLIC
             JUCE_VST3_CAN_REPLACE_VST2=0
-            JUCE_APPLICATION_NAME_STRING="$<TARGET_PROPERTY:${target},JUCE_PRODUCT_NAME>"
-            JUCE_APPLICATION_VERSION_STRING="$<TARGET_PROPERTY:${target},JUCE_VERSION>"
+            JUCE_APPLICATION_NAME_STRING="$<TARGET_PROPERTY:${BV_TARGETCONFIG_TARGET},JUCE_PRODUCT_NAME>"
+            JUCE_APPLICATION_VERSION_STRING="$<TARGET_PROPERTY:${BV_TARGETCONFIG_TARGET},JUCE_VERSION>"
             JUCE_COREGRAPHICS_DRAW_ASYNC=1
             _CRT_SECURE_NO_WARNINGS=1
             JUCE_STRICT_REFCOUNTEDPTR=1
@@ -77,22 +88,24 @@ function (_bv_configure_juce_target target useBrowser use_MTS-ESP forceUseVDSP f
             JUCE_DISABLE_AUDIO_MIXING_WITH_OTHER_APPS=1
             JUCE_EXECUTE_APP_SUSPEND_ON_BACKGROUND_TASK=1)
     
-    _bv_configure_juce_browser (${target} ${useBrowser})
+    _bv_configure_juce_browser (${BV_TARGETCONFIG_TARGET} ${BV_TARGETCONFIG_BROWSER})
 
-    _bv_set_default_macos_options (${target})
+    _bv_set_default_macos_options (${BV_TARGETCONFIG_TARGET})
 
-    target_link_libraries (${target} PUBLIC
+    target_link_libraries (${BV_TARGETCONFIG_TARGET} PUBLIC
         ${BV_JUCE_MODULES}
         juce::juce_recommended_config_flags
         juce::juce_recommended_lto_flags
         juce::juce_recommended_warning_flags)
 
-    target_compile_features (${target} PUBLIC cxx_std_${BV_CXX_VERSION})
+    target_compile_features (${BV_TARGETCONFIG_TARGET} PUBLIC cxx_std_${BV_CXX_VERSION})
 
-    _bv_configure_vecops (${target} ${forceUseVDSP} ${forceIgnoreVDSP})
+    _bv_configure_vecops (${BV_TARGETCONFIG_TARGET} ${BV_TARGETCONFIG_ALWAYS_VDSP} ${BV_TARGETCONFIG_NEVER_VDSP})
 
-    if (${use_MTS-ESP})
-        _bv_configure_mts_esp (${target})
+    if (${BV_TARGETCONFIG_MTS-ESP})
+        _bv_configure_mts_esp (${BV_TARGETCONFIG_TARGET})
     endif()
+
+    set (bv_targetname ${BV_TARGETCONFIG_TARGET} PARENT_SCOPE)
 endfunction()
 
--- cmake/public/public_api.cmake
+++ cmake/public/public_api.cmake
@@ -28,44 +28,22 @@ function (bv_add_resources_folder)
     target_link_libraries (${BV_RSRC_FLDR_TARGET} PRIVATE ${resourcesTarget})
 endfunction()
 
-#
-
-macro (_bv_juce_target_internal_config)
-
-    set (options BROWSER MTS-ESP ALWAYS_VDSP NEVER_VDSP)
-    set (oneValueArgs TARGET)
-    set (multiValueArgs "")
-
-    cmake_parse_arguments (BV_TARGETCONFIG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    if (NOT DEFINED BV_TARGETCONFIG_TARGET)
-        bv_print_warning ("Target name not specified in call to _bv_juce_target_internal_config")
-        return()
-    endif()
-
-    _bv_configure_juce_target (${BV_TARGETCONFIG_TARGET} 
-                               ${BV_TARGETCONFIG_BROWSER}
-                               ${BV_TARGETCONFIG_MTS-ESP}
-                               ${BV_TARGETCONFIG_ALWAYS_VDSP}
-                               ${BV_TARGETCONFIG_NEVER_VDSP})
-endmacro()
-
 
 function (bv_configure_juce_plugin)
-    _bv_juce_target_internal_config (${ARGN})
+    _bv_configure_juce_target (${ARGN})
 
-    target_link_libraries (${BV_TARGETCONFIG_TARGET} PUBLIC ${BV_PLUGIN_ONLY_MODULES})
+    target_link_libraries (${bv_targetname} PUBLIC ${BV_PLUGIN_ONLY_MODULES})
 
-    target_compile_definitions (${BV_TARGETCONFIG_TARGET} PUBLIC
+    target_compile_definitions (${bv_targetname} PUBLIC
             JUCE_MICROPHONE_PERMISSION_ENABLED=1
             JUCE_USE_CUSTOM_PLUGIN_STANDALONE_APP=0)
 endfunction()
 
 
 function (bv_configure_juce_app)
-    _bv_juce_target_internal_config (${ARGN})
+    _bv_configure_juce_target (${ARGN})
 
-    target_link_libraries (${BV_TARGETCONFIG_TARGET} PUBLIC ${BV_APP_ONLY_MODULES})
+    target_link_libraries (${bv_targetname} PUBLIC ${BV_APP_ONLY_MODULES})
 endfunction()
 
 
