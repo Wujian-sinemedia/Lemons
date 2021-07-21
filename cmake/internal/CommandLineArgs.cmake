1d7fce5f632cc13c69dfe9f04a387478ca7cb62f
Ben Vining <ben.the.vining@gmail.com>
2021-07-21 01:54:17 -0700

    updating

--- cmake/internal/CommandLineArgs.cmake
+++
@@ -1,8 +0,0 @@
-if (DEFINED IgnoreVDSP)
-	set (BV_IGNORE_VDSP ${IgnoreVDSP} CACHE INTERNAL "Ignore Apple vDSP for this build?")
-else()
-	set (BV_IGNORE_VDSP FALSE CACHE INTERNAL "Ignore Apple vDSP for this build?")
-endif()
-
-
-# Formats
