dc0974ff7cb2e6ac67d6884ec4488b2f3b9d0523
Ben Vining <ben.the.vining@gmail.com>
2021-07-22 05:25:45 -0700

    Update RealtimeStateObject.h

--- modules/Plugins/bv_plugin_parameters/RealtimeStateObject/RealtimeStateObject.h
+++ modules/Plugins/bv_plugin_parameters/RealtimeStateObject/RealtimeStateObject.h
@@ -6,46 +6,35 @@ template < typename OwnedObjectType >
 class RealtimeStateObject
 {
 public:
-    /*--- realtime write ---*/
+    struct RealtimeScopedWrite;
+    struct RealtimeScopedRead;
+    struct NonrealtimeScopedWrite;
+    struct NonrealtimeScopedRead;
+
+    RealtimeScopedWrite    realtimeWrite();
+    RealtimeScopedRead     realtimeRead();
+    NonrealtimeScopedWrite nonrealtimeWrite();
+    NonrealtimeScopedRead  nonrealtimeRead();
+
+private:
+    friend struct RealtimeScopedWrite;
+    friend struct RealtimeScopedRead;
+    friend struct NonrealtimeScopedWrite;
+    friend struct NonrealtimeScopedRead;
 
     OwnedObjectType& realtime_beginWrite();
     void             realtime_endWrite();
 
-    struct RealtimeScopedWrite;
-
-    RealtimeScopedWrite realtimeWrite();
-
-
-    /*--- realtime read ---*/
-
     const OwnedObjectType& realtime_beginRead();
     void                   realtime_endRead();
-
-    struct RealtimeScopedRead;
-
-    RealtimeScopedRead realtimeRead();
-
-
-    /*--- non realtime write ---*/
 
     OwnedObjectType& nonRealtime_beginWrite();
     void             nonRealtime_endWrite();
 
-    struct NonrealtimeScopedWrite;
-
-    NonrealtimeScopedWrite nonrealtimeWrite();
-
-
-    /*--- non realtime read ---*/
-
     const OwnedObjectType& nonrealtime_beginRead();
     void                   nonrealtime_endRead();
 
-    struct NonrealtimeScopedRead;
 
-    NonrealtimeScopedRead nonrealtimeRead();
-
-private:
     OwnedObjectType object;
 };
 
