11b5a4d8eff1f71fa01a8fe0327ec531809defbc
Ben Vining <ben.the.vining@gmail.com>
2021-08-04 07:49:29 -0700

    SC-50: updating cmake funcs that enable plugin hosting

--- cmake/internal/plugins/plugin_hosting_config.cmake
+++ cmake/internal/plugins/plugin_hosting_config.cmake
@@ -1,8 +1,11 @@
 function (_bv_configure_plugin_hosting target)
 
     target_compile_definitions (${target} PRIVATE 
-    		JUCE_PLUGINHOST_LADSPA=1
-    		JUCE_PLUGINHOST_VST=0
-    		JUCE_PLUGINHOST_VST3=1)
+            JUCE_PLUGINHOST_VST=0
+            JUCE_PLUGINHOST_VST3=1
+    		JUCE_PLUGINHOST_LADSPA=1)
 
+    if (APPLE)
+        target_compile_definitions (${target} PRIVATE JUCE_PLUGINHOST_AU=1)
+    endif()
 endfunction()
