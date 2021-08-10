ede034a698512a5ba738e2f2e3601979a934d673
Ben Vining <ben.the.vining@gmail.com>
2021-08-09 22:02:17 -0700

    made math helper funcs noexcept

--- modules/Common/bv_core/math/mathHelpers.cpp
+++ modules/Common/bv_core/math/mathHelpers.cpp
@@ -1,12 +1,12 @@
 
 namespace bav::math
 {
-bool probability (int percentOfTheTime)
+bool probability (int percentOfTheTime) noexcept
 {
     return juce::Random::getSystemRandom().nextInt (100) < percentOfTheTime;
 }
 
-constexpr int sampsToMs (double samplerate, int numSamples)
+int sampsToMs (double samplerate, int numSamples) noexcept
 {
     jassert (samplerate > 0.0);
     return juce::roundToInt ((numSamples / samplerate) * 1000.0f);
--- modules/Common/bv_core/math/mathHelpers.h
+++ modules/Common/bv_core/math/mathHelpers.h
@@ -4,12 +4,12 @@
 namespace bav::math
 {
 //  returns true a specified percent of the time
-bool probability (int percentOfTheTime);
+bool probability (int percentOfTheTime) noexcept;
 
 
 // returns the period in samples of a specified frequency in hz at a specified samplerate
 template < typename FreqType >
-constexpr int periodInSamples (double samplerate, FreqType freqHz)
+constexpr int periodInSamples (double samplerate, FreqType freqHz) noexcept
 {
     jassert (freqHz > FreqType (0.0));
     return juce::roundToInt (samplerate / freqHz);
@@ -19,7 +19,7 @@ constexpr int periodInSamples (double samplerate, FreqType freqHz)
 // returns the resultant fundamental frequency in Hz from a specified period in samples and samplerate
 template < typename PeriodType >
 constexpr PeriodType freqFromPeriod (double     samplerate,
-                                     PeriodType period)
+                                     PeriodType period) noexcept
 {
     jassert (period > PeriodType (0.0));
 
@@ -31,11 +31,11 @@ constexpr PeriodType freqFromPeriod (double     samplerate,
 
 
 // converts a specified number of samples to milliseconds
-constexpr int sampsToMs (double samplerate, int numSamples);
+int sampsToMs (double samplerate, int numSamples) noexcept;
 
 // converts a specified amount of time in milliseconds to the closest integer number of samples at the specified samplerate
 template < typename msType >
-constexpr int msToSamps (double samplerate, msType ms)
+constexpr int msToSamps (double samplerate, msType ms) noexcept
 {
     return juce::roundToInt (samplerate / 1000.0f * ms);
 }
@@ -43,7 +43,7 @@ constexpr int msToSamps (double samplerate, msType ms)
 
 // converts a midi note to a frequency in Hz
 template < typename noteType >
-constexpr noteType midiToFreq (noteType midiNote)
+constexpr noteType midiToFreq (noteType midiNote) noexcept
 {
     if constexpr (std::is_same_v< noteType, int >)
         return juce::roundToInt (440 * std::pow (2, (midiNote - 69) / 12));
@@ -54,7 +54,7 @@ constexpr noteType midiToFreq (noteType midiNote)
 
 // converts a frequency in Hz to a midi pitch
 template < typename noteType >
-constexpr noteType freqToMidi (noteType freqHz)
+constexpr noteType freqToMidi (noteType freqHz) noexcept
 {
     if constexpr (std::is_same_v< noteType, int >)
         return juce::roundToInt (69 + 12 * log2 (freqHz / 440));
