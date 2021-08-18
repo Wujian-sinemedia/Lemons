69c02c730900626d197fb9b9116f96b6b863daae
Ben Vining <ben.the.vining@gmail.com>
2021-08-18 08:20:52 -0700

    updating

--- cmake/project_repo_config/git.cmake
+++ cmake/project_repo_config/git.cmake
@@ -5,9 +5,3 @@ endif()
 if (NOT GIT_FOUND)
     message (WARNING "Git not found! Some repository configuration features may not work as expected.")
 endif()
-
-#
-
-function (bv_update_repo_gitignore repodir)
-    bv_copy_file (SOURCE ${BV_REPO_CONFIG_FILES_DIR}/default_gitignore.txt DEST ${repodir}/.gitignore)
-endfunction()
