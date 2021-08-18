713f02ab8abd010298ce77de2615b62dee7c937e
Ben Vining <ben.the.vining@gmail.com>
2021-08-18 08:38:22 -0700

    updating

--- cmake/project_repo_config/git.cmake
+++
@@ -1,7 +0,0 @@
-if (NOT GIT_FOUND)
-    find_package (Git QUIET)
-endif()
-
-if (NOT GIT_FOUND)
-    message (WARNING "Git not found! Some repository configuration features may not work as expected.")
-endif()
