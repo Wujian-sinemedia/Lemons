045fe6ec4497bd12e1a0869ae70219d4afa23cae
Ben Vining <ben.the.vining@gmail.com>
2021-08-18 01:04:45 -0700

    updating

--- cmake/project_repo_config/git.cmake
+++ cmake/project_repo_config/git.cmake
@@ -11,12 +11,3 @@ endif()
 function (bv_update_repo_gitignore repodir)
     bv_copy_file (SOURCE ${BV_REPO_CONFIG_FILES_DIR}/default_gitignore.txt DEST ${repodir}/.gitignore)
 endfunction()
-
-#
-
-function (bv_update_repo_git_submodules repodir)
-
-    execute_process (COMMAND ${GIT_EXECUTABLE} submodule foreach --recursive "git fetch && git checkout origin/HEAD && git submodule update"
-                     WORKING_DIRECTORY ${repodir}
-                     OUTPUT_QUIET ERROR_QUIET)
-endfunction()
