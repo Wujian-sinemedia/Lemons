1fa919357da60c405b1a593c7ffb3e0ef59a4696
Ben Vining <ben.the.vining@gmail.com>
2021-08-20 20:52:48 -0700

    Update FileUtilities.cmake

--- cmake/FileUtilities.cmake
+++ cmake/FileUtilities.cmake
@@ -28,32 +28,3 @@ function (bv_subdir_list)
 
   	set (${BV_SUBDIR_RESULT} ${dirlist} PARENT_SCOPE)
 endfunction()
-
-
-function (bv_copy_file)
-
-	set (options "")
-	set (oneValueArgs SOURCE DEST)
-	set (multiValueArgs "")
-
-	cmake_parse_arguments (BV_FILECOPY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-	if (NOT DEFINED BV_FILECOPY_SOURCE)
-		message (WARNING "Source path not defined in call to bv_copy_file")
-		return()
-	endif()
-
-	if (NOT DEFINED BV_FILECOPY_DEST)
-		message (WARNING "Destination path not defined in call to bv_copy_file")
-		return()
-	endif()
-
-	file (READ ${BV_FILECOPY_SOURCE} filetext)
-	file (WRITE ${BV_FILECOPY_DEST} ${filetext})
-endfunction()
-
-
-function (bv_enable_all_file_permissions filepath)
-	file (CHMOD ${filepath}
-          PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_WRITE GROUP_EXECUTE WORLD_READ WORLD_WRITE WORLD_EXECUTE SETUID SETGID)
-endfunction()
