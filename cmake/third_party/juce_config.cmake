379bbfa739dacfa0945a3e3a2312dd6052d523f9
Ben Vining <ben.the.vining@gmail.com>
2021-08-22 17:46:48 -0700

    Update juce_config.cmake

--- cmake/third_party/juce_config.cmake
+++ cmake/third_party/juce_config.cmake
@@ -2,7 +2,16 @@ option (JUCE_ENABLE_MODULE_SOURCE_GROUPS "Use JUCE source groups" ON)
 option (JUCE_BUILD_EXAMPLES "Build JUCE Examples" OFF)
 option (JUCE_BUILD_EXTRAS "Build JUCE Extras" OFF)
 
+if (NOT APPLE AND NOT WIN32)
+        message (STATUS "Using the LV2 Porting Project fork of JUCE.")
+        set (bv_juce_repo https://github.com/lv2-porting-project/JUCE)
+        set (bv_juce_tag origin/lv2)
+else()
+        set (bv_juce_repo https://github.com/juce-framework/JUCE)
+        set (bv_juce_tag  origin/develop)
+endif()
+
 CPMAddPackage (
         NAME JUCE
-        GIT_REPOSITORY https://github.com/juce-framework/JUCE.git
-        GIT_TAG origin/develop)
+        GIT_REPOSITORY ${bv_juce_repo}.git
+        GIT_TAG ${bv_juce_tag})
