5703d45ed517ee0980f910581a677e04ce2fca14
Ben Vining <ben.the.vining@gmail.com>
2021-09-11 22:30:52 -0500

    reorganizing plugin config cmake scripts

--- cmake/plugins/plugin_config.cmake
+++ cmake/plugins/plugin_config.cmake
@@ -40,3 +40,27 @@ function (_bv_configure_juce_lv2 origTarget)
     set_target_properties (${LV2target} PROPERTIES JUCE_LV2_URI "${Company}.${Product}")
 
 endfunction()
+
+###
+
+function (bv_configure_juce_plugin)
+
+    _bv_configure_juce_target (${ARGN})
+
+    target_link_libraries (${bv_targetname} PUBLIC ${BV_PLUGIN_ONLY_MODULES})
+
+    target_compile_definitions (${bv_targetname} PUBLIC
+            JUCE_MICROPHONE_PERMISSION_ENABLED=1
+            JUCE_USE_CUSTOM_PLUGIN_STANDALONE_APP=0)
+
+    _bv_configure_product_install (${bv_targetname} TRUE)
+
+    set (plugins_target "ALL_PLUGINS")
+
+    if (NOT TARGET ${plugins_target})
+        add_custom_target (${plugins_target})
+    endif()
+
+    add_dependencies (${plugins_target} "${bv_targetname}_All")
+
+endfunction()
