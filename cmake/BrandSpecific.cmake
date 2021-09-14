4933958e2ff0a47c95060977ad732a9dce4e4e05
Ben Vining <ben.the.vining@gmail.com>
2021-09-14 18:02:20 -0500

    updating

--- cmake/BrandSpecific.cmake
+++
@@ -1,8 +0,0 @@
-set (BV_DEFAULT_BRAND_FLAGS
-	BUNDLE_ID                   com.BenViningMusicSoftware.${PROJECT_NAME}
-	COMPANY_NAME                BenViningMusicSoftware
-    COMPANY_WEBSITE             www.benvining.com
-    COMPANY_EMAIL               ben.the.vining@gmail.com
-    COMPANY_COPYRIGHT           "Copyright 2021 Ben Vining"
-    PLUGIN_MANUFACTURER_CODE	Benv
-	CACHE INTERNAL "BenViningMusicSoftware brand flags")
