8cb71869c06f0c9e80dd5f8205d0cc81155d26cb
Ben Vining <ben.the.vining@gmail.com>
2021-09-14 02:07:26 -0500

    updating

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -18,23 +18,31 @@ cannot enable two of them in the same build.
 
 ]]
 
+
+if (NOT (CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "MSVC"))
+    message (WARNING "Your compiler (${CMAKE_CXX_COMPILER_ID}) does not have sanitizer support :(")
+    return()
+endif()
+
+
 if (NOT DEFINED BV_SANITIZERS_TO_ENABLE)
 	set (BV_SANITIZERS_TO_ENABLE "")
 endif()
 
 
-macro (check_compiler_version gcc_required_version clang_required_version msvc_required_version)
-
-	if ( (CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${gcc_required_version})
-		OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${clang_required_version})
-		OR (CMAKE_CXX_COMPILER_ID MATCHES "MSVC" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${msvc_required_version}) )
-
-		message (FATAL_ERROR "${sanitizer} could not be enabled, because your compiler (${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}) does not support it!")
-	endif()
-endmacro()
-
-
 macro (enable_sanitizer_flags sanitize_option)
+
+    macro (check_compiler_version gcc_required_version clang_required_version msvc_required_version)
+
+        if ( (CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${gcc_required_version})
+            OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${clang_required_version})
+            OR (CMAKE_CXX_COMPILER_ID MATCHES "MSVC" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${msvc_required_version}) )
+
+            message (FATAL_ERROR "${sanitizer} could not be enabled, because your compiler (${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}) does not support it!")
+        endif()
+    endmacro()
+
+
     if (${sanitize_option} MATCHES "address")
         check_compiler_version ("4.8" "3.1" "19.28")
         if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
@@ -77,30 +85,25 @@ macro (enable_sanitizer_flags sanitize_option)
     endif()
 endmacro()
 
+     
+foreach (sanitizer ${BV_SANITIZERS_TO_ENABLE})
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-        
-    foreach (sanitizer ${BV_SANITIZERS_TO_ENABLE})
+    string (TOLOWER ${sanitizer} sanitizer)
 
-        string (TOLOWER ${sanitizer} sanitizer)
+    enable_sanitizer_flags (${sanitizer})
 
-        enable_sanitizer_flags (${sanitizer})
+    if (CMAKE_C_COMPILER_ID MATCHES "Clang")
+        set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${XSAN_COMPILE_FLAGS}")
+    endif()
 
-        if (CMAKE_C_COMPILER_ID MATCHES "Clang")
-          set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${XSAN_COMPILE_FLAGS}")
-        endif()
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${XSAN_COMPILE_FLAGS}")
 
-        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${XSAN_COMPILE_FLAGS}")
+    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        link_libraries (${XSAN_LINKER_FLAGS})
+    endif()
 
-        if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-          link_libraries (${XSAN_LINKER_FLAGS})
-        endif()
-
-        if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-            string (REPLACE "-Wl,--no-undefined" "" CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
-            string (REPLACE "-Wl,--no-undefined" "" CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
-        endif()
-    endforeach()
-else()
-	message (WARNING "Your compiler (${CMAKE_CXX_COMPILER_ID}) does not have sanitizer support :(")
-endif()
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        string (REPLACE "-Wl,--no-undefined" "" CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
+        string (REPLACE "-Wl,--no-undefined" "" CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
+    endif()
+endforeach()
