e656f9346c5402e1cbf77b208930947637767657
Ben Vining <ben.the.vining@gmail.com>
2021-10-12 04:21:13 -0500

    Update install_windows.sh

--- scripts/install/os/install_windows.sh
+++ scripts/install/os/install_windows.sh
@@ -6,5 +6,8 @@ os_install_func() {
 	local -r DEPS_LIST="$1"
 
 	# install deps listed in deps list file
-	xargs choco install <"$DEPS_LIST"
+	cat "$DEPS_LIST" | while read line || [[ -n $line ]];
+	do
+		choco install "$line"
+	done
 }
