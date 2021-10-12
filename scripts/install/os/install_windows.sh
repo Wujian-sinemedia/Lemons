a0d7e208ccbb74440d0eafc8c7d917ac0d42976c
Ben Vining <ben.the.vining@gmail.com>
2021-10-12 04:19:15 -0500

    Submodule auto-update

--- scripts/install/os/install_windows.sh
+++ scripts/install/os/install_windows.sh
@@ -2,14 +2,9 @@
 
 set -euo pipefail
 
-# TODO: install chocolatey, if needed
-
 os_install_func() {
 	local -r DEPS_LIST="$1"
 
 	# install deps listed in deps list file
-	cat "$DEPS_LIST" | while read line || [[ -n $line ]];
-	do
-		choco install "$line"
-	done
+	xargs choco install <"$DEPS_LIST"
 }
