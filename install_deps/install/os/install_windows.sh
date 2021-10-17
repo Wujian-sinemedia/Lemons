e43cbeb3fb02341636e4ea4a0b914d18e17d1cf7
Ben Vining <ben.the.vining@gmail.com>
2021-10-17 02:27:29 -0500

    Update install_windows.sh

--- install_deps/install/os/install_windows.sh
+++ install_deps/install/os/install_windows.sh
@@ -4,17 +4,29 @@
 
 set -euo pipefail
 
+
+process_line() {
+	local -r line = $(echo "$1" | sed 's/;//')
+
+	for other in "clang-format" "doxygen" "graphviz"
+	do
+		if [ "$line" = "$other" ]; then
+			return
+		fi
+	done
+
+	echo "line : \"$line\""
+
+	choco install "$line"
+}
+
+
 os_install_func() {
 
 	local -r DEPS_LIST="$1"
 
 	cat "$DEPS_LIST" | while read line || [[ -n "$line" ]];
 	do
-		line=$(echo "$line" | sed 's/;//')
-		if [ "$line" = "clang-format" || "$line" = "doxygen" || "$line" = "graphviz" ]; then
-			continue;
-		fi
-		echo "line : \"$line\""
-		choco install "$line"
+		process_line "$line"
 	done
 }
