80811764440bf50a0ec65442ef9045e6433816c0
Ben Vining <ben.the.vining@gmail.com>
2021-10-16 19:15:03 -0500

    Update docs_publish_script.sh

--- .github/docs_publish_script.sh
+++ .github/docs_publish_script.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-
 # install deps
-source "$script_dir/../scripts/install/install_list.sh"
-install_deps_list "doxygen_deps.txt"
-
+bash "$script_dir/../install_deps/install_deps.sh"
 
 # Run python script
 python "$script_dir/publish_docs.py"
-
 
 exit 0
