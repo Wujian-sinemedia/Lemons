582fe9ee2552545d28d4e89f6a205742f6de447d
Ben Vining <ben.the.vining@gmail.com>
2021-10-18 12:55:26 -0500

    updating cmake

--- util/cmake/integrations/clang_format/run_clang_format.cmake
+++ util/cmake/integrations/clang_format/run_clang_format.cmake
@@ -1,35 +1,19 @@
-function (dirlist dir outvar)
-
-	file (GLOB children RELATIVE ${dir} ${dir}/*)
-
-  	set (dirlist "")
-
-  	foreach (child ${children})
-    	if (IS_DIRECTORY ${dir}/${child})
-      		list (APPEND dirlist ${child})
-    	endif()
-  	endforeach()
-
-  	set (${outvar} ${dirlist} PARENT_SCOPE)
-
-endfunction()
+include (@CF_FILEUTILS_SCRIPT@)
 
 #
 
 function (run_clangformat_on_subdir dir)
 
-	dirlist ("${dir}" subdirs)
+	lemons_subdir_list (DIR "${dir}" RESULT subdirs)
 
 	foreach (subdir ${subdirs})
 		run_clangformat_on_subdir ("${subdir}")
 	endforeach()
 
-	execute_process (COMMAND "${CLANG_FORMAT}" -i *.c *.h *.cpp *.hpp
+	execute_process (COMMAND "@CLANG_FORMAT@" -i *.c *.h *.cpp *.hpp
 		             WORKING_DIRECTORY "${dir}")
 endfunction()
 
 #
 
-set (START_DIR "")
-
-run_clangformat_on_subdir ("${START_DIR}")
+run_clangformat_on_subdir ("@LEMONS_TOPLEVEL_DIR@")
