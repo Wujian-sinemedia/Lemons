59dc91e414c0718194d52b64182338c2ebc953e9
Ben Vining <ben.the.vining@gmail.com>
2021-10-20 13:22:44 -0500

    Update install_android_deps.sh

--- util/install_deps/os/install_android_deps.sh
+++ util/install_deps/os/install_android_deps.sh
@@ -2,13 +2,13 @@
 
 sudo apt-get install openjdk-8-jre
 
-sudo update-alternatives --config java
+# sudo update-alternatives --config java
 
 sudo apt-get install openjdk-8-jdk
 
-sudo update-alternatives --config javac
+# sudo update-alternatives --config javac
 
-sudo apt-get install -y android-sdk sdkmanager
+sudo apt-get install android-sdk sdkmanager
 
 yes | sudo sdkmanager --licenses
 
