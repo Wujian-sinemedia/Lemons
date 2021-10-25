4e2d62e2335143dfae54455480b51a41960da9f8
Ben Vining <ben.the.vining@gmail.com>
2021-10-25 15:40:33 -0500

    Update juce_config.cmake

--- util/cmake/third_party/juce_config.cmake
+++ util/cmake/third_party/juce_config.cmake
@@ -3,7 +3,7 @@ option (JUCE_BUILD_EXAMPLES "Build JUCE Examples" OFF)
 option (JUCE_BUILD_EXTRAS "Build JUCE Extras" OFF)
 
 
-option (LEMONS_USE_LV2_JUCE "Use the fork of JUCE that enables compiling to LV2 on Linux" OFF)
+option (LEMONS_USE_LV2_JUCE "Use the fork of JUCE that enables compiling to LV2 on Linux" ON)
 
 if (NOT APPLE AND NOT WIN32 AND LEMONS_USE_LV2_JUCE)
         set (juce_repo https://github.com/lv2-porting-project/JUCE)
