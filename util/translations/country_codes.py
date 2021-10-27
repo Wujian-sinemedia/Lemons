5ef897f8699b19cc0c84a96456f4834abe4a1dc9
Ben Vining <ben.the.vining@gmail.com>
2021-10-27 14:50:31 -0500

    updating

--- util/translations/country_codes.py
+++ util/translations/country_codes.py
@@ -3,6 +3,8 @@
 ###############################################################################
 
 # Returns a list of ISO country codes for a language name
+# supposed to be 2 character ISO compliant codes, separated by whitespace
+# ISO 639-1
 
 def get_country_codes_for_langauge (language):
 	country_codes = []
