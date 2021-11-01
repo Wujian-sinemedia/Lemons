a5fdefe44135e7b6fe0ebfa66dbd1cfe50fd5331
Ben Vining <ben.the.vining@gmail.com>
2021-11-01 14:59:17 -0500

    Update git_uth.cmake

--- util/scripts/git_uth.cmake
+++ util/scripts/git_uth.cmake
@@ -1,22 +1,50 @@
 if (NOT DIR)
+	message (FATAL_ERROR "DIR must be specified!")
 	return()
 endif()
+
+if (NOT IS_DIRECTORY "${DIR}")
+	message (FATAL_ERROR "Requested directory ${DIR} does not exist!")
+	return()
+endif()
+
+#
 
 find_package (Git)
 
 if (NOT Git_FOUND)
+	message (FATAL_ERROR "Git could not be found!")
 	return()
 endif()
 
+#
 
-set (git_branch_name main)
+set (BRANCH_NAME "main" CACHE STRING "The git branch name to use")
 
-set (submodule_update_command submodule update --init --recursive --merge)
+#
 
-execute_process (COMMAND "${GIT_EXECUTABLE}" fetch
-				 COMMAND "${GIT_EXECUTABLE}" pull
-				 COMMAND "${GIT_EXECUTABLE}" ${submodule_update_command}
-				 COMMAND "${GIT_EXECUTABLE}" submodule foreach --recursive 'git checkout ${git_branch_name} && git fetch && git pull && git ${submodule_update_command}'
+cmake_host_system_information (RESULT numCores QUERY NUMBER_OF_LOGICAL_CORES)
+
+set (fetch_command "fetch --all --recurse-submodules --jobs=${numCores}")
+
+set (pull_command "pull --all --recurse-submodules --jobs=${numCores}")
+
+set (submodule_update_command "submodule update --init --recursive")
+
+#
+
+separate_arguments (fetch_command_line UNIX_COMMAND "${fetch_command}")
+
+separate_arguments (pull_command_line UNIX_COMMAND "${pull_command}")
+
+separate_arguments (submodule_update_command_line UNIX_COMMAND "${submodule_update_command}")
+
+separate_arguments (submodule_foreach_command UNIX_COMMAND "submodule foreach --recursive 'git checkout ${BRANCH_NAME} && git ${fetch_command} && git ${pull_command} && git ${submodule_update_command}'")
+
+execute_process (COMMAND "${GIT_EXECUTABLE}" ${fetch_command_line}
+				 COMMAND "${GIT_EXECUTABLE}" ${pull_command_line}
+				 COMMAND "${GIT_EXECUTABLE}" ${submodule_update_command_line}
+				 COMMAND "${GIT_EXECUTABLE}" ${submodule_foreach_command}
 				 COMMAND_ECHO STDOUT
 				 WORKING_DIRECTORY "${DIR}"
 				 OUTPUT_QUIET)
