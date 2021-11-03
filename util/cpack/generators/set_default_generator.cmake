371b0522c4f4d192a449e0d777b0b2e6b686ac8b
Ben Vining <ben.the.vining@gmail.com>
2021-11-02 19:13:57 -0500

    Update set_default_generator.cmake

--- util/cpack/generators/set_default_generator.cmake
+++ util/cpack/generators/set_default_generator.cmake
@@ -11,17 +11,40 @@ if (APPLE)
 endif()
 
 
-# Linux
+# Linux - detect LSB info
+
+find_program (LSB_RELEASE_EXECUTABLE lsb_release)
+
+if (LSB_RELEASE_EXECUTABLE)
+
+    execute_process (COMMAND ${LSB_RELEASE_EXECUTABLE} -sc
+                     OUTPUT_VARIABLE LSB_CODENAME OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process (COMMAND ${LSB_RELEASE_EXECUTABLE} -sr
+                     OUTPUT_VARIABLE LSB_RELEASE OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process (COMMAND ${LSB_RELEASE_EXECUTABLE} -si
+                     OUTPUT_VARIABLE LSB_DISTRIBUTOR_ID OUTPUT_STRIP_TRAILING_WHITESPACE)
+else()
+    message (WARNING "Unable to detect LSB info for your Linux distro")
+
+    set (LSB_DISTRIBUTOR_ID "unknown")
+    set (LSB_RELEASE "unknown")
+    set (LSB_CODENAME "unknown")
+endif() 
+
 
 if (LSB_DISTRIBUTOR_ID MATCHES "Ubuntu")
     set (CPACK_GENERATOR "DEB" CACHE INTERNAL "")
     return()
 endif()
 
+
 if (LSB_DISTRIBUTOR_ID MATCHES "RedHatEnterpriseServer")
     set (CPACK_GENERATOR "RPM" CACHE INTERNAL "")
     return()
 endif()
+
 
 find_program (DEB_EXE debuild)
 
@@ -29,6 +52,7 @@ if (DEB_EXE)
     set (CPACK_GENERATOR "DEB" CACHE INTERNAL "")
     return()
 endif()
+
 
 find_program (RPM_EXE rpmbuild)
 
