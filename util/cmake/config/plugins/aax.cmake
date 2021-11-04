beaa6d1962b095a0455d5650b50aae39bf67ad65
Ben Vining <ben.the.vining@gmail.com>
2021-11-03 22:21:07 -0500

    reorganizing

--- util/cmake/config/plugins/aax.cmake
+++ util/cmake/config/plugins/aax.cmake
@@ -1,3 +1,46 @@
+if (LEMONS_AAX_SDK_PATH AND IS_DIRECTORY "${LEMONS_AAX_SDK_PATH}")
+    
+    if (APPLE AND NOT IOS)
+
+        find_program (XCODE_BUILD xcodebuild)
+
+        if (XCODE_BUILD)
+            add_custom_target (AAXSDK
+                               COMMAND "${XCODE_BUILD}" "-scheme" "AAXLibrary_libcpp" "ONLY_ACTIVE_ARCH=NO" "ARCHS=x86_64" "-configuration" "$<COMMAND_CONFIG:$<CONFIG>>" "build"
+                               COMMAND_EXPAND_LISTS VERBATIM
+                               WORKING_DIRECTORY "${LEMONS_AAX_SDK_PATH}/Libs/AAXLibrary/MacBuild"
+                               COMMENT "Building AAX SDK..."
+                               COMMAND_ECHO STDOUT)
+        endif()
+
+    elseif (WIN32)
+
+        find_program (MS_BUILD msbuild)
+
+        if (MS_BUILD)
+            add_custom_target (AAXSDK
+                               COMMAND "${MS_BUILD}" "AAX_SDK.sln" "-p:Configuration=$<COMMAND_CONFIG:$<CONFIG>>"
+                               COMMAND_EXPAND_LISTS VERBATIM
+                               WORKING_DIRECTORY "${LEMONS_AAX_SDK_PATH}/msvc"
+                               COMMENT "Building AAX SDK..."
+                               COMMAND_ECHO STDOUT)
+        endif()
+    endif()
+endif()
+
+
+if (TARGET AAXSDK)
+    set_target_properties (AAXSDK PROPERTIES OSX_ARCHITECTURES x86_64)
+
+    if (NOT "x86_64" IN_LIST CMAKE_OSX_ARCHITECTURES)
+        message (WARNING "You're not building your shared targets for x86_64, which will cause linker errors with the AAX SDK!")
+    endif()
+endif()
+
+
+#
+
+
 function (_lemons_configure_juce_aax origTarget pagetableFile)
 
     set (aaxTarget ${origTarget}_AAX)
