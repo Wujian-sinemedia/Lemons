cd285ab67be64ed085679af0c6d3a68d763f93c9
Ben Vining <ben.the.vining@gmail.com>
2021-12-01 04:48:27 -0600

    reorganizing

--- modules/Common/LemonsCommonModules.cmake
+++ modules/Common/LemonsCommonModules.cmake
@@ -16,14 +16,30 @@ Common building blocks and utilities
 
 include_guard (GLOBAL)
 
-include (LemonsJuceModuleUtilities)
-
-_lemons_add_module_subcategory (LemonsCommonModules)
-
 #
 
 option (LEMONS_BUILD_TESTS "Build tests" OFF)
 
+include (LemonsJuceModuleUtilities)
+
+set (target LemonsCommonModules)
+
+if (NOT TARGET ${target})
+    add_library (${target} INTERFACE)
+endif()
+
+juce_add_module ("${CMAKE_CURRENT_LIST_DIR}/lemons_core")
+target_link_libraries (${target} INTERFACE lemons_core)
+
 if (LEMONS_BUILD_TESTS)
+    juce_add_module ("${CMAKE_CURRENT_LIST_DIR}/lemons_unit_testing")
+    target_link_libraries (${target} INTERFACE lemons_unit_testing)
+
+    target_link_libraries (lemons_unit_testing INTERFACE
+        juce::juce_recommended_config_flags
+        juce::juce_recommended_lto_flags
+        juce::juce_recommended_warning_flags)
+
     target_compile_definitions (lemons_core INTERFACE LEMONS_UNIT_TESTS=1)
+    target_compile_definitions (lemons_unit_testing INTERFACE LEMONS_UNIT_TESTS=1)
 endif()
