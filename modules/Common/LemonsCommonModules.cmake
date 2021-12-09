5eea618b1e34671312b8bf9ac8e933b5b8e59c66
Ben Vining <ben.the.vining@gmail.com>
2021-12-09 12:09:18 -0600

    Update LemonsCommonModules.cmake

--- modules/Common/LemonsCommonModules.cmake
+++ modules/Common/LemonsCommonModules.cmake
@@ -23,6 +23,14 @@ include (LemonsJuceModuleUtilities)
 
 _lemons_add_module_subcategory (LemonsCommonModules)
 
+target_compile_definitions (lemons_core INTERFACE 
+        LEMONS_VERSION_MAJOR=${Lemons_VERSION_MAJOR} 
+        LEMONS_VERSION_MINOR=${Lemons_VERSION_MINOR} 
+        LEMONS_VERSION_PATCH=${Lemons_VERSION_PATCH}
+        LEMONS_PROJECT_VERSION_MAJOR=${PROJECT_VERSION_MAJOR}
+        LEMONS_PROJECT_VERSION_MINOR=${PROJECT_VERSION_MINOR}
+        LEMONS_PROJECT_VERSION_PATCH=${PROJECT_VERSION_PATCH})
+
 if (LEMONS_BUILD_TESTS)
     target_compile_definitions (lemons_core INTERFACE LEMONS_UNIT_TESTS=1)
 endif()
