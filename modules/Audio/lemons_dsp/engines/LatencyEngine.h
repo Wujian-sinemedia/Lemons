1c010263315a0adc7977a4fde5f4e8592c886215
Ben Vining <ben.the.vining@gmail.com>
2021-12-09 20:26:25 -0600

    updating

--- modules/Audio/lemons_dsp/engines/LatencyEngine.h
+++ modules/Audio/lemons_dsp/engines/LatencyEngine.h
@@ -13,13 +13,14 @@ public:
 	/** Returns the latency in samples of the engine. */
 	int reportLatency() const final { return internalBlocksize; }
 
-	/** Sets the latency in samples of the engine. */
-	void changeLatency (int newInternalBlocksize);
-
 	/** Sets the samplerate of the engine.
 	    This function exists so that you can update only the samplerate, without needing to call Engine::prepare() and provide the blocksize again.
 	 */
 	void setSamplerate (double samplerate);
+    
+protected:
+    /** Sets the latency in samples of the engine. */
+    void changeLatency (int newInternalBlocksize);
 
 private:
 	void renderBlock (const AudioBuffer<SampleType>& input, AudioBuffer<SampleType>& output, MidiBuffer& midiMessages, bool isBypassed) final;
