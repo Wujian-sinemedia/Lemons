bf9b14720ef0ece542616071593b5bba2605fc68
Ben Vining <ben.the.vining@gmail.com>
2022-01-10 01:03:57 -0600

    updating templates

--- modules/GUI/lemons_gui/utilities/util.h
+++ modules/GUI/lemons_gui/utilities/util.h
@@ -5,13 +5,13 @@ namespace lemons::gui::util
 
 using juce::Component;
 
-static void addAndMakeVisible (Component& parent, Component& child)
+static inline void addAndMakeVisible (Component& parent, Component& child)
 {
 	parent.addAndMakeVisible (child);
 }
 
 template <typename... Args>
-static void addAndMakeVisible (Component& parent, Component& firstChild, Args&&... rest)
+static inline void addAndMakeVisible (Component& parent, Component& firstChild, Args&&... rest)
 {
 	addAndMakeVisible (parent, firstChild);
 	addAndMakeVisible (parent, std::forward<Args> (rest)...);
--- util/Templates/app/Source/main.cpp
+++ util/Templates/app/Source/main.cpp
@@ -1,12 +1,9 @@
 #include <new_app_gui/new_app_gui.h>
-#include <lemons_app_utils/lemons_app_utils.h>
 
-struct NewApp : lemons::GuiApp< New::GUI >
+
+namespace New
 {
-    NewApp()
-    : GuiApp< New::GUI > ("NewApp", "0.0.1", {1150, 700})
-    {
-    }
-};
+using App = lemons::GuiApp< GUI >;
+}
 
-START_JUCE_APPLICATION (NewApp)
+START_JUCE_APPLICATION (New::App)
