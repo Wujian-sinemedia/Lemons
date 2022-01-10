2b0f7cfce6d8ded02283e0e5cdd67664d645da30
Ben Vining <ben.the.vining@gmail.com>
2022-01-10 03:31:42 -0600

    made compiling all modules individually into a CTest step

---
+++ util/tests/CompileModulesIndividually/RunModulesBuild.cmake
@@ -0,0 +1,28 @@
+
+set (config_output_file @CMAKE_CURRENT_BINARY_DIR@/logs/moduleBuild_Config.log)
+set (config_error_output @CMAKE_CURRENT_BINARY_DIR@/logs/moduleBuild_ConfigError.log)
+set (build_output_file @CMAKE_CURRENT_BINARY_DIR@/logs/moduleBuild_Build.log)
+set (build_error_output @CMAKE_CURRENT_BINARY_DIR@/logs/moduleBuild_BuildError.log)
+
+file (MAKE_DIRECTORY @CMAKE_CURRENT_BINARY_DIR@/logs)
+file (TOUCH ${config_output_file} ${config_error_output} ${build_output_file} ${build_error_output})
+
+
+message (STATUS " --- configuring individual module builds... ---")
+
+execute_process (COMMAND @CMAKE_COMMAND@ -B Builds -G @CMAKE_GENERATOR@
+				 WORKING_DIRECTORY @CMAKE_CURRENT_LIST_DIR@/CompileModulesIndividually
+				 COMMAND_ECHO STDOUT
+				 COMMAND_ERROR_IS_FATAL ANY
+				 OUTPUT_FILE ${config_output_file}
+				 ERROR_FILE ${config_error_output})
+
+
+message (STATUS " --- building each module individually... ---")
+
+execute_process (COMMAND @CMAKE_COMMAND@ --build Builds
+				 WORKING_DIRECTORY @CMAKE_CURRENT_LIST_DIR@/CompileModulesIndividually
+				 COMMAND_ECHO STDOUT
+				 COMMAND_ERROR_IS_FATAL ANY
+				 OUTPUT_FILE ${build_output_file}
+				 ERROR_FILE ${build_error_output})
