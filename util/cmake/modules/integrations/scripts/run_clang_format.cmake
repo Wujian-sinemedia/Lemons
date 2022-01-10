ae161054f68b618c590bead816d3565f63f00457
Ben Vining <ben.the.vining@gmail.com>
2022-01-09 23:43:20 -0600

    Update run_clang_format.cmake

--- util/cmake/modules/integrations/scripts/run_clang_format.cmake
+++ util/cmake/modules/integrations/scripts/run_clang_format.cmake
@@ -1,6 +1,6 @@
 #!/usr/bin/env cmake -P
 
-include (@Lemons_SOURCE_DIR@/AddCMakeModules.cmake)
+include (@Lemons_SOURCE_DIR@/scripts/AddCMakeModules.cmake)
 
 list (APPEND CMAKE_MODULE_PATH ${LEMONS_CMAKE_MODULE_PATH})
 
