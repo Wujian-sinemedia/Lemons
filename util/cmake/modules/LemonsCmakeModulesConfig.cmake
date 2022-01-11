25efa761e4b4c0483b2cbcc918560fa13572a249
Ben Vining <ben.the.vining@gmail.com>
2022-01-10 21:52:05 -0600

    updating

--- util/cmake/modules/LemonsCmakeModulesConfig.cmake
+++ util/cmake/modules/LemonsCmakeModulesConfig.cmake
@@ -4,10 +4,16 @@ cmake_minimum_required (VERSION 3.21 FATAL_ERROR)
 
 file (GLOB children RELATIVE ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_LIST_DIR}/*)
 
+list (REMOVE_ITEM children Builds)
+
 foreach (child ${children})
 	set (full_path "${CMAKE_CURRENT_LIST_DIR}/${child}")
 
 	if (IS_DIRECTORY "${full_path}")
+		if ("${full_path}" STREQUAL "${CMAKE_CURRENT_BINARY_DIR}")
+			continue()
+		endif()
+
 		file (REAL_PATH "${full_path}" _abs_path EXPAND_TILDE)
 		list (APPEND lemonsModulePaths "${_abs_path}")
 	endif()
--- util/cmake/modules/plugins/LemonsAddPrivateSDKs.cmake
+++ util/cmake/modules/plugins/LemonsAddPrivateSDKs.cmake
@@ -2,6 +2,10 @@ include_guard (GLOBAL)
 
 include (LemonsGetCPM)
 
+if (CPM_PrivateSDKs_SOURCE)
+        file (REAL_PATH ${CPM_PrivateSDKs_SOURCE} CPM_PrivateSDKs_SOURCE EXPAND_TILDE)
+endif()
+
 CPMAddPackage (
         NAME PrivateSDKs
         GITHUB_REPOSITORY benthevining/PrivateSDKs
