7def62e37b4e3e20283c8c3532f33a795a9d2852
Ben Vining <ben.the.vining@gmail.com>
2022-01-23 22:18:47 -0600

    Update publish_docs.sh

--- .github/publish_docs.sh
+++ .github/publish_docs.sh
@@ -42,7 +42,7 @@ rm -rf -- *
 
 # copy generated docs to cloned copy of docs git tree
 # shellcheck disable=SC2086
-mv $lemons_root/util/doxygen/doc/* "$docs_git_tree"
+mv "$lemons_root/util/doxygen/doc"/* "$docs_git_tree"
 
 # configure git
 git config push.default simple
