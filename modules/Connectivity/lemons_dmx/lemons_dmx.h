14e301a68b7c6f2dbc830d8c098528a7db0d2894
Ben Vining <ben.the.vining@gmail.com>
2022-02-03 08:43:02 -0600

    chore: initial commit of files for new DMX module
    Precommit-Verified: 9c316a5e296aa7eecdf884bbdb3baab802954d49d33fb3a4167438a1a3d1072b

---
+++ modules/Connectivity/lemons_dmx/lemons_dmx.h
@@ -0,0 +1,38 @@
+
+/*
+ * ======================================================================================
+ *
+ *  ██╗     ███████╗███╗   ███╗ ██████╗ ███╗   ██╗███████╗
+ *  ██║     ██╔════╝████╗ ████║██╔═══██╗████╗  ██║██╔════╝
+ *  ██║     █████╗  ██╔████╔██║██║   ██║██╔██╗ ██║███████╗
+ *  ██║     ██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║╚════██║
+ *  ███████╗███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║███████║
+ *  ╚══════╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+ *
+ *  This file is part of the Lemons open source library and is licensed under the terms of the GNU Public License.
+ *
+ * ======================================================================================
+ */
+
+#pragma once
+
+/*-------------------------------------------------------------------------------------
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+ ID:                 lemons_dmx
+ vendor:             Lemons
+ version:            0.0.1
+ name:               lemons_dmx
+ description:        Utilities for working with the DMX512 protocol
+ website:            http://benthevining.github.io/Lemons/
+ license:            GPL-3.0
+ minimumCppStandard: 17
+ dependencies:       lemons_core
+
+ END_JUCE_MODULE_DECLARATION
+
+ -------------------------------------------------------------------------------------*/
+
+
+#include "util/util.h"
---
+++ modules/Connectivity/lemons_dmx/util/util.h
@@ -0,0 +1,31 @@
+/*
+ * ======================================================================================
+ *
+ *  ██╗     ███████╗███╗   ███╗ ██████╗ ███╗   ██╗███████╗
+ *  ██║     ██╔════╝████╗ ████║██╔═══██╗████╗  ██║██╔════╝
+ *  ██║     █████╗  ██╔████╔██║██║   ██║██╔██╗ ██║███████╗
+ *  ██║     ██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║╚════██║
+ *  ███████╗███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║███████║
+ *  ╚══════╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+ *
+ *  This file is part of the Lemons open source library and is licensed under the terms of the GNU Public License.
+ *
+ * ======================================================================================
+ */
+
+#pragma once
+
+namespace lemons::dmx
+{
+
+[[nodiscard]] constexpr bool isValidChannel (int channel)
+{
+	return channel > 0 && channel <= 512;
+}
+
+[[nodiscard]] constexpr bool isValidChannelValue (int value)
+{
+	return value >= 0 && value <= 255;
+}
+
+}  // namespace lemons::dmx
