8c928d3701428b83863cb56a83ed7ef905b7c40b
Ben Vining <ben.the.vining@gmail.com>
2022-02-11 16:21:28 -0600

    refactor: updating algorithms
    Precommit-Verified: 56dd0c68e3e541a66c167cc4848b95b04aa32e9cc8990dfd795901a168104b90

--- modules/Common/lemons_core/misc/Algorithm.h
+++ modules/Common/lemons_core/misc/Algorithm.h
@@ -52,10 +52,12 @@ template <class ContainerType, class T, class UnaryPredicate>
 	return *res;
 }
 
-template <class T, class ContainerType, class UnaryPredicate,
-		  std::enable_if_t<std::is_default_constructible_v<T>>* = nullptr>
+template <class T, class ContainerType, class UnaryPredicate>
 [[nodiscard]] constexpr T contains_or_default (const ContainerType& container, UnaryPredicate&& p)
 {
+	static_assert (std::is_default_constructible_v<T>,
+				   "T must be default constructible!");
+
 	const auto res = std::find_if (container.begin(), container.end(), std::move (p));
 
 	if (res == container.end())
@@ -81,17 +83,45 @@ template <class ContainerType, class UnaryPredicate>
 	return static_cast<int> (std::count_if (container.begin(), container.end(), std::move (p)));
 }
 
-template <class T, class ContainerType, class UnaryPredicate, class UnaryFunc1, class UnaryFunc2>
-constexpr void call_or (const ContainerType& container, UnaryPredicate&& p, UnaryFunc1&& f1, UnaryFunc2&& f2)
+template <class ContainerType>
+constexpr void removeDuplicates (ContainerType& container)
 {
-	std::for_each (container.begin(), container.end(),
-				   [pred = std::move (p), yesFunc = std::move (f1), noFunc = std::move (f2)] (const T& obj)
-				   {
-					   if (pred (obj))
-						   yesFunc (obj);
-					   else
-						   noFunc (obj);
-				   });
+	container.erase (std::unique (container.begin(), container.end()), container.end());
+}
+
+template <class ContainerType>
+constexpr void reverse (ContainerType& container)
+{
+	std::reverse (container.begin(), container.end());
+}
+
+template <class ContainerType>
+constexpr void sort (ContainerType& container, bool forward = true)
+{
+	std::sort (container.begin(), container.end());
+
+	if (! forward)
+		reverse (container);
+}
+
+template <class ContainerType, class Comparison>
+constexpr void sort (ContainerType& container, Comparison&& predicate, bool forward = true)
+{
+	std::sort (container.begin(), container.end(), std::move (predicate));
+
+	if (! forward)
+		reverse (container);
+}
+
+template <class Container1, class Container2, class Callable>
+constexpr void call_both (const Container1& container1, const Container2& container2, Callable&& callable)
+{
+	for (auto first = container1.begin(), second = container2.begin();
+		 first != container1.end() && second != container2.end();
+		 ++first, ++second)
+	{
+		callable (*first, *second);
+	}
 }
 
 }  // namespace lemons::alg
